ef17998e9f6a9c2eef5e11e5484d985529dc0861
Serhii Shyian <serhii.shn@gmail.com>
2023-08-02 16:38:55 +0300

    1) Created init_db.sql file in src/main/resources folder 2) In pom.xml added mysql-connector-java dependency 3) Created ConnectionUtil class in util package 4) Created Book class in model package 5) Created DataProcessingException class in exception package 6) Created BookDao class and its implementation in dao package 7) In Main class tested all methods from BookService class

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,46 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
+import java.math.BigDecimal;
 
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao =
+                (BookDao) injector.getInstance(BookDao.class);
+        Book kobzar = new Book(
+                "Kobzar",
+                BigDecimal.valueOf(599));
+        Book eneida = new Book(
+                "Eneida",
+                BigDecimal.valueOf(259));
+        Book mavka = new Book(
+                "Mavka",
+                BigDecimal.valueOf(419));
+
+        System.out.println("START TESTING:");
+        System.out.println("BookService create method was called.");
+        System.out.println(bookDao.create(kobzar));
+        System.out.println(bookDao.create(eneida));
+        System.out.println(bookDao.create(mavka));
+        System.out.println("Done");
+        System.out.println("BookService findById method was called.");
+        System.out.println(bookDao.findById(eneida.getId()));
+        System.out.println("Done");
+        System.out.println("BookService update method was called.");
+        eneida.setPrice(BigDecimal.valueOf(379));
+        System.out.println(bookDao.update(eneida));
+        System.out.println("Done");
+        System.out.println("BookService delete method was called.");
+        bookDao.deleteById(3L);
+        System.out.println("Done");
+        System.out.println("BookService findAll method was called.");
+        System.out.println(bookDao.findAll());
+        System.out.println("Done");
+        System.out.println("FINISH TESTING:");
     }
 }
