6b935ece7ae36d71034823302bfb34eedd436bf8
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-08-03 12:08:31 +0300

    added custom exception and initial db

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `library_db`.`books` (
+                                      `id` INT NOT NULL AUTO_INCREMENT,
+                                      `title` VARCHAR(45) NULL,
+                                      `price` BIGINT NULL,
+                                      PRIMARY KEY (`id`));
