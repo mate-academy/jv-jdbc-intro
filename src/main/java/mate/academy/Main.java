48918a8e2ac352c76016c8b999c2ab2d64fb7191
Oleg Ravlyk <oleg.ravlyk2005@gmail.com>
2023-08-03 13:29:32 +0300

    implemented dao

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,21 +1,28 @@
 package mate.academy;
 
+import mate.academy.dao.BookDao;
+import mate.academy.dao.impl.BookDaoImpl;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 import mate.academy.util.ConnectionUtil;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        Connection connection = ConnectionUtil.getConnection();
-        Statement statement = null;
-        try {
-            statement = connection.createStatement();
-            statement.execute("INSERT INTO books (title, price) VALUES ('dsa', 77)");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = bookDao.create(new Book("Interesting BOOK", BigDecimal.valueOf(777)));
+        System.out.println(book.getId());
+        System.out.println(bookDao.findById(book.getId()));
+        System.out.println(bookDao.findAll());
+        book.setTitle("BORING");
+        bookDao.update(book);
+        System.out.println(bookDao.deleteById(book.getId()));
 
     }
 }
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,5 +1,7 @@
-CREATE TABLE `library_db`.`books` (
-                                      `id` INT NOT NULL AUTO_INCREMENT,
-                                      `title` VARCHAR(45) NULL,
-                                      `price` BIGINT NULL,
-                                      PRIMARY KEY (`id`));
+CREATE TABLE `books` (
+                         `id` bigint NOT NULL AUTO_INCREMENT,
+                         `title` varchar(45) DEFAULT NULL,
+                         `price` bigint DEFAULT NULL,
+                         `is_deleted` tinyint NOT NULL DEFAULT '0',
+                         PRIMARY KEY (`id`)
+)
