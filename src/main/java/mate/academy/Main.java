3ccc04d16e07c661e7b90e0a1ca89489d8da5985
Volodymyr Mykychak <leadervlo.d@gmail.com>
2023-08-07 15:30:14 +0300

    check some bugs

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -21,5 +21,16 @@ public class Main {
         // Set the literary format of the book using the LiteraryFormatService
         book.setPrice(BigDecimal.valueOf(50));
         bookService.createBook(book);
+        Book book1 = new Book();
+        book.setTitle("Java");
+        book.setPrice(BigDecimal.valueOf(200));
+        bookService.createBook(book1);
+        bookService.getAllBooks();
+        Book book2 = new Book();
+        book2.setId(2L);
+        book2.setPrice(BigDecimal.valueOf(150));
+        bookService.updateBook(book2);
+        bookService.getAllBooks();
+        bookService.deleteBook(3L);
     }
 }
