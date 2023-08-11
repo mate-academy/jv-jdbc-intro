33d99657f545724509c905bf9c34c3f9a9182aa8
DaniilNepelev <kakashilvng@gmail.com>
2023-08-11 14:19:41 +0300

    done solution

---
+++ src/main/java/mate/academy/exception/DataException.java
@@ -0,0 +1,7 @@
+package mate.academy.exception;
+
+public class DataException extends RuntimeException{
+    public DataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
