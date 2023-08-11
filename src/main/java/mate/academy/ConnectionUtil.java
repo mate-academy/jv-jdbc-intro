fa717a805781b99917afb351bbda04cae4babcdf
Ihor <kalaurdelta96@gmail.com>
2023-08-11 17:07:22 +0300

    added solution

---
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package mate.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String DB_URL =  "jdbc:mysql://localhost:3306/my_first_library_db";
+    private static final String MY_SQL_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER_VALUE = "root";
+    private static final String PASSWORD_VALUE = "180296.ua";
+    private static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", USER_VALUE);
+        DB_PROPERTIES.put("password", PASSWORD_VALUE);
+        try {
+            Class.forName(MY_SQL_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load: " + MY_SQL_DRIVER, e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_PROPERTIES);
+    }
+}
---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,135 @@
+package mate.academy.dao;
+
+import mate.academy.ConnectionUtil;
+import mate.academy.exceptions.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Book;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Dao
+public class BookDaoImpl implements BookDao{
+    private static final int INDEX_OF_FIRST_PARAM = 1;
+    private static final int INDEX_OF_SECOND_PARAM = 2;
+    private static final int INDEX_OF_THIRD_PARAM = 3;
+
+    @Override
+    public Book create(Book book) {
+        String sqlQuery = "INSERT INTO books (title, price) VALUES (?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement =
+                     connection.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(INDEX_OF_FIRST_PARAM, book.getTitle());
+            statement.setObject(INDEX_OF_SECOND_PARAM, book.getPrice());
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new RuntimeException("Inserting failure");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                book.setId(id);
+            }
+
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't add new book: " + book, e);
+        }
+        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        String sqlQuery = "SELECT * FROM books WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
+
+            statement.setLong(INDEX_OF_FIRST_PARAM, id);
+            ResultSet resultSet = statement.executeQuery();
+            Book book = null;
+
+            while (resultSet.next()) {
+                String title = resultSet.getString("title");
+                BigDecimal price = resultSet.getBigDecimal("price");
+
+                book = new Book(id, title, price);
+            }
+
+            return Optional.ofNullable(book);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to the DB", e);
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        List<Book> books = new ArrayList<>();
+        String sqlQuery = "SELECT * FROM books";
+
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sqlQuery);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String title = resultSet.getString("title");
+                BigDecimal price = resultSet.getBigDecimal("price");
+
+                Book book = new Book(id, title, price);
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Error while finding all books", e);
+        }
+
+        return books;
+    }
+
+    @Override
+    public Book update(Book book) {
+        String sqlQuery = "UPDATE books SET title = ?, price = ? WHERE id = ?";
+
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
+
+            statement.setString(INDEX_OF_FIRST_PARAM, book.getTitle());
+            statement.setObject(INDEX_OF_SECOND_PARAM, book.getPrice());
+            statement.setLong(INDEX_OF_THIRD_PARAM, book.getId());
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new RuntimeException("Updating failure, expect");
+            }
+
+            return book;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't update book: " + book, e);
+        }
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        String sqlQuery = "DELETE FROM books WHERE id = ?";
+
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sqlQuery)) {
+            statement.setLong(1, id);
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows < 1) {
+                return  false;
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't delete book with ID: " + id, e);
+        }
+        return true;
+    }
+}
