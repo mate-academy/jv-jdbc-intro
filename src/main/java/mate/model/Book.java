29a4fbfb2211db4f5c469d41eac4bdfec37534fe
Andriy Sichniy <sichniy.andriy@gmail.com>
2023-08-11 14:41:51 +0300

    add Book class

---
+++ src/main/java/mate/model/Book.java
@@ -0,0 +1,15 @@
+package mate.model;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+}
