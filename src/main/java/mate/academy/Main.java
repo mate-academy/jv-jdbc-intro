15c78f6ff1b1c9edf24a426647af1757486b88db
Ihor Vasylenko <vasylenkoihor23@gmail.com>
2023-08-11 17:15:10 +0300

    implemented task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,25 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
+import java.math.BigDecimal;
+import java.util.Optional;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("Sample");
+        book.setPrice(BigDecimal.valueOf(5.99));
+        bookDao.create(book);
+        Long bookId = 1L;
+        Optional<Book> foundBook = bookDao.findById(bookId);
+        bookDao.findAll();
+        Book updatedBook = foundBook.orElseThrow();
+        bookDao.update(updatedBook);
+        bookDao.deleteById(bookId);
     }
 }
