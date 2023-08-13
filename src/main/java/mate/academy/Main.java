64b1a3e06b818f326bbdf386745ef521555fa51a
Tetiana <tetiana.kolomiiets.fop@gmail.com>
2023-08-13 21:10:25 +0300

    Initial commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,29 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    private static final Injector injector = Injector.getInstance("YOUR_PACKAGE");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("Sherlock Holmes");
+        book.setPrice(BigDecimal.valueOf(400));
+        Book createdBook = bookDao.create(book);
+        Optional<Book> foundBook = bookDao.findById(1L);
+        List<Book> allBooks = bookDao.findAll();
+        Book updatedBook = new Book();
+        updatedBook.setTitle("The Adventures of Sherlock Holmes");
+        updatedBook.setPrice(BigDecimal.valueOf(500));
+        Book newBook = bookDao.update(updatedBook);
+        bookDao.deleteById(1L);
     }
 }
---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,123 @@
+package mate.academy.dao;
+
+import mate.academy.ConnectionUtil;
+import mate.academy.exceptions.DataProcessingException;
+import mate.academy.model.Book;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BookDaoImpl implements BookDao {
+    private static final int ID_INDEX = 1;
+    private static final int TITLE_INDEX = 2;
+    private static final int PRICE_INDEX = 3;
+
+    @Override
+    public Book create(Book book) {
+        String sql = "INSERT INTO books (title, price) VALUES (?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(TITLE_INDEX, book.getTitle());
+            statement.setBigDecimal(PRICE_INDEX, book.getPrice());
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows < 1) {
+                throw new RuntimeException("Expected to insert at least one row, but inserted 0 rows.");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(ID_INDEX, Long.class);
+                book.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't create new book: " + book, e);
+        }
+        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        String sql = "SELECT * FROM books WHERE id = ?";
+
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(ID_INDEX, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Book book = extractBook(resultSet, id);
+                return Optional.of(book);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not create a connection to the DB", e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        String sql = "SELECT * FROM books";
+        List<Book> books = new ArrayList<>();
+
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()) {
+                Long id = resultSet.getObject("id", Long.class);
+                Book book = extractBook(resultSet, id);
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not create a connection to the DB", e);
+        }
+        return books;
+    }
+
+    @Override
+    public Book update(Book book) {
+        String sql = "UPDATE books SET title = ?, price = ? WHERE id = ?";
+
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, book.getTitle());
+            statement.setBigDecimal(2, book.getPrice());
+            statement.setLong(3, book.getId());
+            if (statement.executeUpdate() < 1) {
+                throw new RuntimeException("Expected to update at least one row, but updated 0 rows.");
+            }
+
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not create a connection to the DB", e);
+        }
+        return book;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        String sql = "DELETE FROM books WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(ID_INDEX, id);
+            return statement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not create a connection to the DB", e);
+        }
+    }
+
+    private static Book extractBook(ResultSet resultSet, Long id) throws SQLException {
+        String title = resultSet.getString("title");
+        BigDecimal price = resultSet.getObject("price", BigDecimal.class);
+        Book book = new Book();
+        book.setId(id);
+        book.setTitle(title);
+        book.setPrice(price);
+        return book;
+    }
+}
