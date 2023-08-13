8eea86b949aec4a59b635da9d0f0a529109a9e0c
Mykyta Zuber <nikitazuber@gmail.com>
2023-08-13 21:24:47 +0300

    fixed code style

--- src/main/java/mate/academy/ConnectionUtil.java
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -12,7 +12,7 @@ public class ConnectionUtil {
     static {
         DB_PROPERTIES = new Properties();
         DB_PROPERTIES.put("user", "root");
-        DB_PROPERTIES.put("password", "S13o28N24m23$");
+        DB_PROPERTIES.put("password", "1234Root");
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
