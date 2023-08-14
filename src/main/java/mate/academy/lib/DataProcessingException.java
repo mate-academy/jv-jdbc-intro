ce598d8257adf2549da0f2e4ab7b124730ef367f
Valerii <kulibchenkovalera@gmail.com>
2023-08-14 13:15:55 +0300

    second commit

--- src/main/java/mate/academy/lib/DataProcessingException.java
+++ src/main/java/mate/academy/lib/DataProcessingException.java
@@ -1,11 +1,7 @@
 package mate.academy.lib;
 
 public class DataProcessingException extends RuntimeException {
-    private String message;
-    private Throwable ex;
-
-    public DataProcessingException(String message, Throwable ex) {
-        this.message = message;
-        this.ex = ex;
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
