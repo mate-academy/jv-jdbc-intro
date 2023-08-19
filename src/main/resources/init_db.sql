2bd8dd3614b178641d590cf0b345bcd355f4607c
a1111 <okhrymovych11@gmail.com>
2023-08-19 19:51:34 +0300

    Commit JDBC Intro First

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA IF NOT EXISTS `books_db` DEFAULT CHARACTER SET utf8 ;
+USE `books_db` ;
+CREATE TABLE IF NOT EXISTS `books_db`.`books` (
+                                                  `id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                  `title` VARCHAR(255) NULL,
+                                                  `price` DECIMAL UNSIGNED NULL,
+                                                  PRIMARY KEY (`id`));
