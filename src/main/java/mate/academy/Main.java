07c81b023dfaab8f436e24e321286b4ae1349061
Oleksandr Domnich <dmnichz@gmail.com>
2023-08-21 16:47:14 +0300

    added solution for jv-jdbc-intro task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,25 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("Book number one");
+        book.setPrice(BigDecimal.valueOf(11.11));
+        book = bookDao.create(book);
+        System.out.println(book);
+        System.out.println(bookDao.findById(book.getId()));
+        book.setTitle("Book number two");
+        book.setPrice(BigDecimal.valueOf(22.22));
+        System.out.println(bookDao.update(book));
+        System.out.println(bookDao.deleteById(book.getId()));
+        System.out.println(bookDao.findAll());
     }
 }
