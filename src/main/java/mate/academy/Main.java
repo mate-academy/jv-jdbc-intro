2e6d380c6252dbc2ee656e04064b378838b32384
Vitalii <vitaliyplys1@gmail.com>
2023-08-25 18:39:08 +0300

    EXAMPLE

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,32 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
+import java.math.BigDecimal;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book book1 = new Book();
+        book1.setTitle("Book1");
+        book1.setPrice(BigDecimal.valueOf(100));
+
+        Book book1Created = bookDao.create(book1);
+        System.out.println(bookDao.findById(book1Created.getId()));
+
+        Book book2 = new Book();
+        book2.setTitle("Book2");
+        book2.setPrice(BigDecimal.valueOf(150));
+        Book book2Created = bookDao.create(book2);
+        System.out.println(bookDao.findById(book2Created.getId()));
+
+        book2.setTitle("Book3");
+        bookDao.update(book2);
+        bookDao.deleteById(book1Created.getId());
+        System.out.println(bookDao.findAll());
     }
 }
