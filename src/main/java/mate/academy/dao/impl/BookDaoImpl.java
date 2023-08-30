b80ee05b7aea0be94d17f1b53108d8196ca58d3a
Danylo Borysenko <borysenkodanyl@gmail.com>
2023-08-30 15:33:47 +0300

    BookDaoImpl class was created

--- src/main/java/mate/academy/dao/impl/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/BookDaoImpl.java
@@ -1,34 +1,114 @@
 package mate.academy.dao.impl;
 
-import mate.academy.dao.BookDao;
-import mate.academy.model.Book;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import mate.academy.dao.BookDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Book;
+import mate.academy.util.ConnectionUtil;
 
+@Dao
 public class BookDaoImpl implements BookDao {
     @Override
     public Book create(Book book) {
-        return null;
+        String createBookStatement = "INSERT INTO books (title, price) VALUES(?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(createBookStatement,
+                        Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, book.getTitle());
+            preparedStatement.setBigDecimal(2, book.getPrice());
+            preparedStatement.executeUpdate();
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                book.setId(generatedKeys.getLong(1));
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not create book" + book, e);
+        }
+        return book;
     }
 
     @Override
     public Optional<Book> findById(Long id) {
-        return Optional.empty();
+        Book book = null;
+        String findByIdStatement = "SELECT * FROM books WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(findByIdStatement)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                book = parseBookFromResultSet(resultSet);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not find book by id" + id, e);
+        }
+        return Optional.ofNullable(book);
     }
 
     @Override
     public List<Book> findAll() {
-        return null;
+        List<Book> books = new ArrayList<>();
+        String findAllStatement = "SELECT * FROM books";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(findAllStatement)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                books.add(parseBookFromResultSet(resultSet));
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not find all books", e);
+        }
+        return books;
     }
 
     @Override
     public Book update(Book book) {
-        return null;
+        String updateBookStatement = "UPDATE books SET title = ?, price = ? WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(updateBookStatement)) {
+            preparedStatement.setString(1, book.getTitle());
+            preparedStatement.setBigDecimal(2, book.getPrice());
+            preparedStatement.setLong(3, book.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not update book" + book, e);
+        }
+        return book;
     }
 
     @Override
     public boolean deleteById(Long id) {
-        return false;
+        String deleteBookStatement = "DELETE FROM books WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement =
+                        connection.prepareStatement(deleteBookStatement)) {
+            preparedStatement.setLong(1, id);
+            return preparedStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not delete book by id" + id, e);
+        }
+    }
+
+    private Book parseBookFromResultSet(ResultSet resultSet) {
+        Book book = new Book();
+        try {
+            book.setId(resultSet.getObject(1, Long.class));
+            book.setPrice(resultSet.getBigDecimal("price"));
+            book.setTitle(resultSet.getString("title"));
+            return book;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can not parse book from resultSet", e);
+        }
     }
 }
