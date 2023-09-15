28e8e95714fb9a4d0614ee72f110945f7e856c26
Tetiana Koval <tetyanak0102@gmail.com>
2023-09-15 15:46:45 +0300

    added BookDao interface and its implementation

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,30 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book book = new Book();
+        book.setTitle("Shining");
+        book.setPrice(new BigDecimal("1.11"));
+
+        bookDao.create(book);
+
+        Optional<Book> bookById = bookDao.findById(1L);
+        List<Book> books = bookDao.findAll();
+
+        book.setPrice(new BigDecimal("2.22"));
+        bookDao.update(book);
+
+        bookDao.deleteById(1L);
     }
 }
