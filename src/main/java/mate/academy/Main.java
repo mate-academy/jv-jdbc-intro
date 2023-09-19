a7cc7a356c9c3ec7f4d475412aa42fb13480c872
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-09-19 18:43:38 +0300

    impoved main prints

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -14,17 +14,17 @@ public class Main {
         BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
         BookService bookService = new BookServiceImpl(bookDao);
 
-        System.out.println(bookService.findById(1L));
+        System.out.println("Find by id 1 " + bookService.findById(1L));
 
         Book newBook = new Book("Цветы для Элджернона", BigDecimal.valueOf(350));
         Book createdBook = bookService.create(newBook);
-        System.out.println(createdBook);
+        System.out.println("Created book " + createdBook);
 
         Book bookToUpdate = new Book(3L, "Тёмные начала", BigDecimal.valueOf(500));
         Book updated = bookService.update(bookToUpdate);
-        System.out.println(updated + "\n");
+        System.out.println("Updated book " + updated);
 
-        System.out.println(bookService.deleteById(2L));
+        System.out.println("Is deleted book by id 2 " + bookService.deleteById(2L));
 
         for (Book book : bookService.findAll()) {
             System.out.println(book);
