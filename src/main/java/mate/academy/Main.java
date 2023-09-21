9d5ba80482f86ee1c58d79e49538449e508d4c9f
Volodymyr Skvira <deruty13061999@gmail.com>
2023-09-21 20:52:31 +0300

    Implemented solution for the task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,23 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    public static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book firstBook = new Book("Ulysses", BigDecimal.valueOf(25.99));
+        Book secondBook = new Book("Don Quixote", BigDecimal.valueOf(35.50));
+        bookDao.create(firstBook);
+        bookDao.create(secondBook);
+        bookDao.findAll();
+        bookDao.findById(1L);
+        firstBook.setTitle("The Great Gatsby");
+        bookDao.update(firstBook);
+        bookDao.deleteById(2L);
     }
 }
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE test;
+USE test;
+CREATE TABLE books
+(
+    id    BIGINT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255)   NOT NULL,
+    price DECIMAL(12, 2) NOT NULL
+);
