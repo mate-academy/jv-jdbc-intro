c931d96be67351089bed259c522e94b693667356
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-09-25 06:01:13 -0500

    Solution was added: created ConnectionUtil class responsible for connection, BookDao and BookDaoImpl responsible for CRUD operations and custom exception

---
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -0,0 +1,21 @@
+package mate.academy.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/test";
+    private static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", "root");
+        DB_PROPERTIES.put("password", "Passw0rdRO0Tpassvvord");
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_PROPERTIES);
+    }
+}
