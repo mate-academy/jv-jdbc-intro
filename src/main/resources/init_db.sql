7771c8b9fac338abd0de9d61934e72b791eb4d26
Denis Bohusevich <bogusevich03@gmail.com>
2023-09-25 10:21:22 +0300

    jdbc2

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,7 +1,1 @@
 USE init_db;
-
-CREATE TABLE books (
-                       id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                       title VARCHAR(255) NOT NULL,
-                       price DECIMAL(10, 2)
-);
