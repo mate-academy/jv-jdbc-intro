0ee14f6eb66b1fe6a15e1ce8023d3976c531f37c
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-09-26 00:41:10 +0300

    Removed extra lines

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -15,17 +15,13 @@ public class Main {
         Book secondBook = new Book("Martin Eden", new BigDecimal(200));
         Book thirdBook = new Book("Thinking in Java", new BigDecimal(600));
         Book fourthBook = new Book("The Godfather", new BigDecimal(500));
-
         bookDao.create(firstBook);
         bookDao.create(secondBook);
         bookDao.create(thirdBook);
         bookDao.create(fourthBook);
-
         bookDao.update(new Book(secondBook.getId(),
                 "John BarleyCorn", new BigDecimal(250)));
-
         bookDao.deleteById(4L);
-
         System.out.println(bookDao.findById(1L));
         System.out.println(bookDao.findAll());
     }
