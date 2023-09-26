a4cfd9482586fed6f5bbd1bdc920cabee0493839
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-09-26 20:23:39 +0300

    Added solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,45 @@
 package mate.academy;
 
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.dao.BookDao;
+import mate.academy.dao.BookDaoImpl;
+import mate.academy.dao.Injector;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.model.Book;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
 
+        BookDao bookDao = (BookDaoImpl) injector.getInstance(BookDao.class);
+        List<Book> books = List.of(
+                new Book("PIPIXIA is a shrimp", BigDecimal.valueOf(111.00)),
+                new Book("Dungeon master", BigDecimal.valueOf(300.00)),
+                new Book("Average java enjoyer", BigDecimal.valueOf(150.50)),
+                new Book("Lil JJ: Java Jebrony", BigDecimal.valueOf(9999.99))
+        );
+
+        List<Book> createdBooks = books.stream()
+                .map(bookDao::create)
+                .toList();
+
+        Book book = bookDao.findById(createdBooks.get(1).getId())
+                .orElseThrow(() -> new DataProcessingException("Book was not found"));
+        System.out.println(" ");
+        System.out.println(book);
+
+        System.out.println(" ");
+        bookDao.findAll().forEach(System.out::println);
+
+        book.setTitle("Java Survival Evolved");
+        Book updateBook = bookDao.update(book);
+        System.out.println(" ");
+        System.out.println(updateBook);
+
+        bookDao.deleteById(createdBooks.get(0).getId());
+        System.out.println(" ");
+        bookDao.findAll().forEach(System.out::println);
     }
 }
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS test;
+USE test;
+CREATE TABLE IF NOT EXISTS book (id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                  title VARCHAR(255) NOT NULL,
+                                  price DECIMAL(10, 2) NOT NULL);
