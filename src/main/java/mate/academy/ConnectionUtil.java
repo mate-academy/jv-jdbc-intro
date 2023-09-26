a824bdbf87aef9df139c16115ba38df1def31796
Pavlo Stefiniv <pastcwdt@gmail.com>
2023-09-26 09:55:29 +0300

    My impl of jdbc!

---
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -0,0 +1,23 @@
+package mate.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+    }
+}
