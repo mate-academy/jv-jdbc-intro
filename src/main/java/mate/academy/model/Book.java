ee0ef15d04b486bcb8394fbccc015412fb37c6fd
Dmytro Zarudnyi <dmytro.zarudnyi@gmail.com>
2023-10-09 22:01:29 +0300

    set project main structure, added some method implementations

---
+++ src/main/java/mate/academy/model/Book.java
@@ -0,0 +1,27 @@
+package mate.academy.model;
+
+import java.math.BigDecimal;
+
+public class Book {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+
+    public Book(Long id, String title, BigDecimal price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
