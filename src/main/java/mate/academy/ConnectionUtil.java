a50cbc6f763dfdf127013e5c05aff3f8a0b6d3ba
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2023-10-26 13:49:52 +0300

    solved task jdbc-intro: added constants.

--- src/main/java/mate/academy/ConnectionUtil.java
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -8,14 +8,19 @@ import java.util.Properties;
 public class ConnectionUtil {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/mate_academy_hibernate";
     private static final Properties DB_PROPERTIES;
+    private static final String MYSQL_JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String PROPERTY_USER = "user";
+    private static final String PROPERTY_PASSWORD = "password";
+    private static final String USER_NAME = "root";
+    private static final String USER_PASSWORD = "root";
 
     static {
         DB_PROPERTIES = new Properties();
-        DB_PROPERTIES.put("user", "root");
-        DB_PROPERTIES.put("password", "root");
+        DB_PROPERTIES.put(PROPERTY_USER, USER_NAME);
+        DB_PROPERTIES.put(PROPERTY_PASSWORD, USER_PASSWORD);
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(MYSQL_JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver.", e);
         }
