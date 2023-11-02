8ff7cfe0ca65edc195661bf8ee3e3290d63cb45a
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2023-11-02 16:44:20 +0200

    Fixed checkstyle

--- src/main/java/mate/academy/dao/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -1,18 +1,18 @@
 package mate.academy.dao;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import mate.academy.exceptions.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Book;
 import mate.academy.utils.ConnectionUtil;
-import java.math.BigDecimal;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Dao
 public class BookDaoImpl implements BookDao {
@@ -25,14 +25,16 @@ public class BookDaoImpl implements BookDao {
     @Override
     public Book create(Book book) {
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS)) {
+                PreparedStatement statement = connection.prepareStatement(SQL_CREATE,
+                        Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, book.getTitle());
             statement.setBigDecimal(2, book.getPrice());
 
             int affectedRows = statement.executeUpdate();
 
             if (affectedRows < 1) {
-                throw new RuntimeException("Expected to insert at least 1 row, actual: " + affectedRows);
+                throw new RuntimeException("Expected to insert at least 1 row, actual: "
+                        + affectedRows);
             }
 
             ResultSet generatedKeys = statement.getGeneratedKeys();
@@ -51,7 +53,7 @@ public class BookDaoImpl implements BookDao {
     public Optional<Book> findById(Long id) {
         Book book = null;
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(SQL_FIND_BY_ID)) {
+                PreparedStatement statement = connection.prepareStatement(SQL_FIND_BY_ID)) {
             statement.setLong(1, id);
 
             ResultSet resultSet = statement.executeQuery();
@@ -73,7 +75,7 @@ public class BookDaoImpl implements BookDao {
     public List<Book> findAll() {
         List<Book> books = new ArrayList<>();
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(SQL_FIND_ALL)) {
+                PreparedStatement statement = connection.prepareStatement(SQL_FIND_ALL)) {
             ResultSet allBooks = statement.executeQuery();
 
             while (allBooks.next()) {
@@ -98,14 +100,14 @@ public class BookDaoImpl implements BookDao {
         BigDecimal price = book.getPrice();
 
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(SQL_UPDATE)) {
-            Optional<Book> oldBook = findById(id);
-
+                PreparedStatement statement = connection.prepareStatement(SQL_UPDATE)) {
             statement.setString(1, title);
             statement.setBigDecimal(2, price);
             statement.setLong(3, id);
 
             statement.executeUpdate();
+
+            Optional<Book> oldBook = findById(id);
 
             return oldBook.orElseThrow(()
                     -> new RuntimeException("Book is not exist"));
@@ -117,7 +119,7 @@ public class BookDaoImpl implements BookDao {
     @Override
     public boolean deleteById(Long id) {
         try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(SQL_DELETE)) {
+                PreparedStatement statement = connection.prepareStatement(SQL_DELETE)) {
             statement.setLong(1, id);
 
             int affectedRows = statement.executeUpdate();
--- src/main/java/mate/academy/model/Book.java
+++ src/main/java/mate/academy/model/Book.java
@@ -48,10 +48,10 @@ public class Book {
 
     @Override
     public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", price=" + price +
-                '}';
+        return "Book{"
+                + "id=" + id
+                + ", title='" + title
+                + '\'' + ", price=" + price
+                + '}';
     }
 }
