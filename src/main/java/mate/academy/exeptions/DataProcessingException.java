93a200462cee529da8ab21e2300af649b63850d3
Solomia Bobyak <mia.bobyak@gmail.com>
2023-11-03 15:25:00 +0200

    add directory

--- src/main/java/mate/academy/exeptions/DataProcessingException.java
+++ src/main/java/mate/academy/exeptions/DataProcessingException.java
@@ -1,4 +1,4 @@
-package mate.academy;
+package mate.academy.exeptions;
 
 public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String message, Throwable ex) {
