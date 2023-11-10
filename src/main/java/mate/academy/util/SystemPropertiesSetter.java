724d3893efd8f52b11e147546e6108f5ee0d6cb1
Mykhailo Dymusha <mykhailo.dymusha@gmail.com>
2023-11-10 17:17:46 +0200

    Fixed imports.

---
+++ src/main/java/mate/academy/util/SystemPropertiesSetter.java
@@ -0,0 +1,8 @@
+package mate.academy.util;
+
+public class SystemPropertiesSetter {
+    public static void setSystemProperties() {
+        System.setProperty("user", "root");
+        System.setProperty("password", "956874lv");
+    }
+}
