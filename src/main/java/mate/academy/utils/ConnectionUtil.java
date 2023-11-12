50f3e36db8c2792570eab32220d67937bc84ba5d
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-11-12 17:19:00 +0200

    removed credentials

--- src/main/java/mate/academy/utils/ConnectionUtil.java
+++ src/main/java/mate/academy/utils/ConnectionUtil.java
@@ -11,8 +11,8 @@ public class ConnectionUtil {
 
     static {
         DB_PROPERTIES = new Properties();
-        DB_PROPERTIES.put("user","root");
-        DB_PROPERTIES.put("password","07121990@Vg");
+        DB_PROPERTIES.put("user","");
+        DB_PROPERTIES.put("password","");
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
