ee366e824fbaad7394c285fca420974b346a40a3
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-11-13 08:42:33 +0200

    added constants and util methods

--- src/main/java/mate/academy/dao/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -16,21 +16,27 @@ import mate.academy.utils.ConnectionUtil;
 
 @Dao
 public class BookDaoImpl implements BookDao {
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_PRICE = "price";
+    private static final int PARAM_INDEX_ONE = 1;
+    private static final int PARAM_INDEX_TWO = 2;
+    private static final int PARAM_INDEX_THREE = 1;
+    private static final int MIN_AFFECTED_ROWS = 1;
     @Override
     public Book create(Book book) {
         String sql = "INSERT INTO book (title, price) VALUES (?, ?)";
+        String errorMessage = "Expected to insert at least one row";
         try (Connection connection = ConnectionUtil.getConnection();
                  PreparedStatement statement = connection.prepareStatement(sql,
                         Statement.RETURN_GENERATED_KEYS)) {
-            statement.setString(1, book.getTitle());
-            statement.setBigDecimal(2, book.getPrice());
+            statement.setString(PARAM_INDEX_ONE, book.getTitle());
+            statement.setBigDecimal(PARAM_INDEX_TWO, book.getPrice());
             int affectedRows = statement.executeUpdate();
-            if (affectedRows < 1) {
-                throw new RuntimeException("Expected to insert at least one row");
-            }
+            checkAffectedRows(affectedRows,errorMessage);
             ResultSet generatedKeys = statement.getGeneratedKeys();
             if (generatedKeys.next()) {
-                Long id = generatedKeys.getObject(1, Long.class);
+                Long id = generatedKeys.getObject(PARAM_INDEX_ONE, Long.class);
                 book.setId(id);
             }
         } catch (SQLException e) {
@@ -44,15 +50,12 @@ public class BookDaoImpl implements BookDao {
         String sql = "SELECT * FROM BOOK WHERE id =?";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setLong(1, id);
+            statement.setLong(PARAM_INDEX_ONE, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                String title = resultSet.getString("title");
-                BigDecimal price = BigDecimal.valueOf(resultSet.getObject("price", Double.class));
-                Book book = new Book();
-                book.setId(id);
-                book.setTitle(title);
-                book.setPrice(price);
+                String title = resultSet.getString(COLUMN_TITLE);
+                BigDecimal price = BigDecimal.valueOf(resultSet.getObject(COLUMN_PRICE, Double.class));
+                Book book = createBookObject(id, title, price);
                 return Optional.of(book);
             }
         } catch (SQLException e) {
@@ -70,13 +73,10 @@ public class BookDaoImpl implements BookDao {
                 PreparedStatement statement = connection.prepareStatement(sql)) {
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                Long id = resultSet.getObject("id", Long.class);
-                String title = resultSet.getString("title");
-                BigDecimal price = BigDecimal.valueOf(resultSet.getObject("price", Double.class));
-                Book book = new Book();
-                book.setId(id);
-                book.setTitle(title);
-                book.setPrice(price);
+                Long id = resultSet.getObject(COLUMN_ID, Long.class);
+                String title = resultSet.getString(COLUMN_TITLE);
+                BigDecimal price = BigDecimal.valueOf(resultSet.getObject(COLUMN_PRICE, Double.class));
+                Book book = createBookObject(id, title, price);
                 list.add(book);
             }
         } catch (SQLException e) {
@@ -88,16 +88,15 @@ public class BookDaoImpl implements BookDao {
     @Override
     public Book update(Book book) {
         String sql = "UPDATE book SET title = ?, price = ? WHERE id = ?";
+        String errorMessage = "Expected to update at least 1 row";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql,
                         Statement.RETURN_GENERATED_KEYS)) {
-            statement.setLong(3, book.getId());
-            statement.setString(1, book.getTitle());
-            statement.setBigDecimal(2, book.getPrice());
+            statement.setLong(PARAM_INDEX_THREE, book.getId());
+            statement.setString(PARAM_INDEX_ONE, book.getTitle());
+            statement.setBigDecimal(PARAM_INDEX_TWO, book.getPrice());
             int affectedRows = statement.executeUpdate();
-            if (affectedRows < 1) {
-                throw new RuntimeException("Expected to update at least 1 row");
-            }
+            checkAffectedRows(affectedRows, errorMessage);
             return book;
         } catch (SQLException e) {
             throw new DataProcessingException("Cant update the book: " + book, e);
@@ -109,11 +108,24 @@ public class BookDaoImpl implements BookDao {
         String sql = "DELETE FROM book WHERE id = ?";
         try (Connection connection = ConnectionUtil.getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setLong(1, id);
+            statement.setLong(PARAM_INDEX_ONE, id);
             int affectedRows = statement.executeUpdate();
             return affectedRows > 0;
         } catch (SQLException e) {
             throw new DataProcessingException("Can't delete book with given id: " + id, e);
         }
+    }
+    private Book createBookObject(Long id, String title, BigDecimal price) {
+        Book book = new Book();
+        book.setId(id);
+        book.setTitle(title);
+        book.setPrice(price);
+        return book;
+    }
+
+    private boolean checkAffectedRows(int rows, String message) throws RuntimeException{
+        if(rows < MIN_AFFECTED_ROWS) {
+            throw new RuntimeException(message);
+        }else return true;
     }
 }
--- src/main/java/mate/academy/utils/ConnectionUtil.java
+++ src/main/java/mate/academy/utils/ConnectionUtil.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/books";
+    private static final String CLASS_NAME = "com.mysql.cj.jdbc.Driver";
     private static final Properties DB_PROPERTIES;
 
     static {
@@ -15,7 +16,7 @@ public class ConnectionUtil {
         DB_PROPERTIES.put("password","");
 
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName(CLASS_NAME);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("Can't load JDBC driver", e);
         }
