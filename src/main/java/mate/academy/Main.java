06e050a64261db97cca1d287d93ade96c51c1f7f
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-11-13 14:21:56 +0200

    added some changes after the code review

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,6 +2,7 @@ package mate.academy;
 
 import java.math.BigDecimal;
 import mate.academy.dao.BookDao;
+import mate.academy.dao.BookDaoImpl;
 import mate.academy.lib.Injector;
 import mate.academy.model.Book;
 
@@ -9,7 +10,7 @@ public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
-        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        BookDao bookDao = (BookDao) injector.getInstance(BookDaoImpl.class);
         Book book = new Book();
         book.setPrice(BigDecimal.valueOf(9.99));
         book.setTitle("Return of the King");
