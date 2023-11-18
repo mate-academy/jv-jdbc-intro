47989b47f34ab85fb73ba7c13fa2a2f028101e77
Oleksii Borysenko <343502597s@gmail.com>
2023-11-18 18:59:44 +0100

    checkstyle

--- src/main/java/mate/academy/dao/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -8,10 +8,10 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import mate.academy.util.ConnectionUtil;
 import mate.academy.lib.Dao;
 import mate.academy.model.Book;
 import mate.academy.service.DataProcessingException;
+import mate.academy.util.ConnectionUtil;
 
 @Dao
 public class BookDaoImpl implements BookDao {
