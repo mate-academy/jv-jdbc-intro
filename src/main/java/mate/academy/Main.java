f44e171484be00fd519ba2fd6fcd282754ad6a95
marinakusniruk <Kushnirukmv@gmail.com>
2023-11-22 20:19:29 +0100

    jv-jdbc-intro

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -3,7 +3,6 @@ package mate.academy;
 import mate.academy.dao.BookDao;
 import mate.academy.lib.Injector;
 import mate.academy.model.Book;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
