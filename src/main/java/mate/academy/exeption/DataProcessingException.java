45dd10c88cf2136611c6115b21203e279aacfe38
Roman Globa <globaroman@gmail.com>
2023-11-24 22:18:10 +0200

    Implemented DAO layer to Book and other cases in  accordance with tasks

---
+++ src/main/java/mate/academy/exeption/DataProcessingException.java
@@ -0,0 +1,12 @@
+package mate.academy.exeption;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message) {
+        super(message);
+    }
+
+    public DataProcessingException(String message, Throwable cause) {
+
+        super(message, cause);
+    }
+}
