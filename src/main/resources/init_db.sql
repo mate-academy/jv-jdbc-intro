88890bed65217ba13a86573279e45d2dbba4494f
User <Monsterhunter@ukr.net>
2023-11-27 22:17:24 +0200

    created init_db.sql file where I put my table script, then created Book class with all getters and setters(and the constructor), then created BookDao interface with all options I need, then created class BookDaoImpl where I override all my options from BookDao interface, now in Main class I can call all CRUD methods.

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,1 +1,5 @@
-SELECT * FROM books;
+CREATE TABLE books (
+                       book_id INT AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(255) NOT NULL,
+                       price VARCHAR(50)
+);
