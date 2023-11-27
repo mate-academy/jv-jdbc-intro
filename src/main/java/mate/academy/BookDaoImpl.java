ca8e66f72fda9a16b49f0556dec723d96eb1f74a
User <Monsterhunter@ukr.net>
2023-11-27 20:28:06 +0200

    created init_db.sql file where I put my table script, then created Book class with all getters and setters(and the constructor), then created BookDao interface with all options I need, then created class BookDaoImpl where I override all my options from BookDao interface, now in Main class I can call all CRUD methods.

---
+++ src/main/java/mate/academy/BookDaoImpl.java
@@ -0,0 +1,169 @@
+package mate.academy;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.lib.BookDao;
+import mate.academy.lib.Dao;
+
+@Dao
+public class BookDaoImpl implements BookDao {
+
+    @Override
+    public Book create(Book book) {
+        String sql = "INSERT INTO Books (id, title, price) VALUES (?, ?, ?)";
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet generatedKeys = null;
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            statement.setLong(1, book.getId());
+            statement.setString(2, book.getTitle());
+            statement.setBigDecimal(3, book.getPrice());
+
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows < 1) {
+                throw new RuntimeException(
+                        "Expected to insert at least one row, but inserted 0 rows");
+            }
+
+            generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                book.setId(id);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't add new Book: " + book, e);
+
+        } finally {
+            closeResources(connection, statement, generatedKeys);
+        }
+        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        String sql = "SELECT id, title, price FROM Books WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String title = Optional.ofNullable(resultSet.getString("title")).orElse("");
+                BigDecimal price = resultSet.getBigDecimal("price");
+
+                return Optional.of(new Book(id, title, price));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while finding Book by id: " + id, e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        List<Book> books = new ArrayList<>();
+        String sql = "SELECT id, title, price FROM Books";
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String title = resultSet.getString("title");
+                BigDecimal price = resultSet.getBigDecimal("price");
+                books.add(new Book(id, title, price));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while finding all books", e);
+
+        } finally {
+            closeResources(connection, statement, resultSet);
+        }
+        return books;
+    }
+
+    @Override
+    public Book update(Book updatedBook) {
+        String sql = "UPDATE Books SET title = ?, price = ? WHERE id = ?";
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, updatedBook.getTitle());
+            statement.setBigDecimal(2, updatedBook.getPrice());
+            statement.setLong(3, updatedBook.getId());
+
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows < 1) {
+                throw new RuntimeException("No book found with id: " + updatedBook.getId());
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while updating Book: " + updatedBook, e);
+
+        } finally {
+            closeResources(connection, statement, null);
+        }
+        return updatedBook;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        String sql = "DELETE FROM Books WHERE id = ?";
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setLong(1, id);
+
+            int affectedRows = statement.executeUpdate();
+            return affectedRows > 0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while deleting Book by id: " + id, e);
+
+        } finally {
+            closeResources(connection, statement, null);
+        }
+    }
+
+    private void closeResources(Connection connection, Statement statement, ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error closing database resources", e);
+        }
+    }
+}
--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,27 @@
 package mate.academy;
 
+import mate.academy.lib.BookDao;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
+        BookDao bookDao = new BookDaoImpl();
 
+        Long bookIdToUpdate = 1L;
+
+        Optional<Book> foundBook = bookDao.findById(bookIdToUpdate);
+
+        if (foundBook.isPresent()) {
+            Book bookToUpdate = foundBook.get();
+            bookToUpdate.setPrice(new BigDecimal(200));
+
+            Book updatedBook = bookDao.update(bookToUpdate);
+
+            System.out.println("Updated Book: " + updatedBook);
+        } else {
+            System.out.println("No book found with ID: " + bookIdToUpdate);
+        }
     }
 }
