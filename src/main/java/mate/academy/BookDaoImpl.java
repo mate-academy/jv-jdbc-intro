59ead527e929306b73634580c3409c11860c518b
User <Monsterhunter@ukr.net>
2023-11-28 22:48:34 +0200

    created init_db.sql file where I put my table script, then created Book class with all getters and setters(and the constructor), then created BookDao interface with all options I need, then created class BookDaoImpl where I override all my options from BookDao interface, now in Main class I can call all CRUD methods.

--- src/main/java/mate/academy/BookDaoImpl.java
+++ src/main/java/mate/academy/BookDaoImpl.java
@@ -18,9 +18,13 @@ public class BookDaoImpl implements BookDao {
     @Override
     public Book create(Book book) {
         String sql = "INSERT INTO Books (id, title, price) VALUES (?, ?, ?)";
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement =
-                     connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet generatedKeys = null;
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             statement.setLong(1, book.getId());
             statement.setString(2, book.getTitle());
@@ -33,15 +37,17 @@ public class BookDaoImpl implements BookDao {
                         "Expected to insert at least one row, but inserted 0 rows");
             }
 
-            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    Long id = generatedKeys.getObject(1, Long.class);
-                    book.setId(id);
-                }
+            generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                book.setId(id);
             }
 
         } catch (SQLException e) {
             throw new RuntimeException("Can't add new Book: " + book, e);
+
+        } finally {
+            closeResources(connection, statement, generatedKeys);
         }
         return book;
     }
@@ -49,22 +55,29 @@ public class BookDaoImpl implements BookDao {
     @Override
     public Optional<Book> findById(Long id) {
         String sql = "SELECT id, title, price FROM Books WHERE id = ?";
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
 
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.prepareStatement(sql);
             statement.setLong(1, id);
 
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    String title = Optional.ofNullable(resultSet.getString("title")).orElse("");
-                    BigDecimal price = resultSet.getBigDecimal("price");
-                    return Optional.of(new Book(id, title, price));
-                }
+            resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String title = Optional.ofNullable(resultSet.getString("title")).orElse("");
+                BigDecimal price = resultSet.getBigDecimal("price");
+                return Optional.of(new Book(id, title, price));
             }
 
         } catch (SQLException e) {
             throw new RuntimeException("Error while finding Book by id: " + id, e);
+
+        } finally {
+            closeResources(connection, statement, resultSet);
         }
+
         return Optional.empty();
     }
 
@@ -72,9 +85,14 @@ public class BookDaoImpl implements BookDao {
     public List<Book> findAll() {
         List<Book> books = new ArrayList<>();
         String sql = "SELECT id, title, price FROM Books";
-        try (Connection connection = ConnectionUtil.getConnection();
-             Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(sql)) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(sql);
 
             while (resultSet.next()) {
                 Long id = resultSet.getLong("id");
@@ -85,6 +103,9 @@ public class BookDaoImpl implements BookDao {
 
         } catch (SQLException e) {
             throw new RuntimeException("Error while finding all books", e);
+
+        } finally {
+            closeResources(connection, statement, resultSet);
         }
         return books;
     }
@@ -92,9 +113,12 @@ public class BookDaoImpl implements BookDao {
     @Override
     public Book update(Book updatedBook) {
         String sql = "UPDATE Books SET title = ?, price = ? WHERE id = ?";
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
+        Connection connection = null;
+        PreparedStatement statement = null;
 
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.prepareStatement(sql);
             statement.setString(1, updatedBook.getTitle());
             statement.setBigDecimal(2, updatedBook.getPrice());
             statement.setLong(3, updatedBook.getId());
@@ -107,6 +131,9 @@ public class BookDaoImpl implements BookDao {
 
         } catch (SQLException e) {
             throw new RuntimeException("Error while updating Book: " + updatedBook, e);
+
+        } finally {
+            closeResources(connection, statement, null);
         }
         return updatedBook;
     }
@@ -114,9 +141,12 @@ public class BookDaoImpl implements BookDao {
     @Override
     public boolean deleteById(Long id) {
         String sql = "DELETE FROM Books WHERE id = ?";
-        try (Connection connection = ConnectionUtil.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
+        Connection connection = null;
+        PreparedStatement statement = null;
 
+        try {
+            connection = ConnectionUtil.getConnection();
+            statement = connection.prepareStatement(sql);
             statement.setLong(1, id);
 
             int affectedRows = statement.executeUpdate();
@@ -124,6 +154,25 @@ public class BookDaoImpl implements BookDao {
 
         } catch (SQLException e) {
             throw new RuntimeException("Error while deleting Book by id: " + id, e);
+
+        } finally {
+            closeResources(connection, statement, null);
+        }
+    }
+
+    private void closeResources(Connection connection, Statement statement, ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error closing database resources", e);
         }
     }
 }
