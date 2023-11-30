3ada6b8643f9b1b8cb4c2479d364bafeb4e117dc
Vasyl Palokha <xaski514@gmail.com>
2023-11-30 18:31:43 +0200

    JDBC HW

---
+++ src/main/java/mate/academy/dao/BookDao.java
@@ -0,0 +1,17 @@
+package mate.academy.dao;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.model.Book;
+
+public interface BookDao {
+    Book create(Book book);
+
+    List<Book> findAll();
+
+    Book update(Book book);
+
+    Optional<Book> findById(Long id);
+
+    boolean deletedById(Long id);
+}
