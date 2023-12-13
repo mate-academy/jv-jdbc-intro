6a5ebc9a96b00139b3fce9c9b6968ff25b5adcb8
MateuszRostek <17rostek17@gmail.com>
2023-12-13 21:17:58 +0100

    BookDao, ConnectionUtil, custom exception and database with table created

--- src/main/java/mate/academy/util/ConnectionUtil.java
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -1,5 +1,30 @@
 package mate.academy.util;
 
+import mate.academy.exceptions.DataProcessingException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class ConnectionUtil {
-    private static final String DB_URL
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/bookstore";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Lq1725W8Zzws";
+    private static Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", USERNAME);
+        DB_PROPERTIES.put("password", PASSWORD);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new DataProcessingException("Can't load MYSQL Driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_PROPERTIES);
+    }
 }
