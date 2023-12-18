dc715532bbc8445d79ce753c23539b3a33bd3ec4
TkachukTara <zaz42988@gmail.com>
2023-12-18 15:29:25 +0200

    Done HW!

---
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -0,0 +1,28 @@
+package mate.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/test";
+    private static final Properties dbProperties;
+
+    static {
+        dbProperties = new Properties();
+        dbProperties.put("user", "root");
+        dbProperties.put("password", "Taras312");
+        try {
+            Class.forName(DRIVER_CLASS_NAME);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can not loud JDBC driver: " + e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, dbProperties);
+    }
+
+}
