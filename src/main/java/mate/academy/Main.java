23e77fd0e9ad37ce11f9b71cc113d0fd2c28c032
Olga Starkova <ole4kastar79@gmail.com>
2023-12-19 01:08:44 +0200

    fixed CI Bild Errors:import separation,long string

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -3,7 +3,6 @@ package mate.academy;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-
 import mate.academy.dao.BookDao;
 import mate.academy.lib.Injector;
 import mate.academy.model.Book;
@@ -16,19 +15,19 @@ public class Main {
     private static final BigDecimal BOOK_TEST_PRICE_NEW = BigDecimal.valueOf(14.00);
 
     public static void main(String[] args) {
-        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
         Book starWars = new Book();
         starWars.setId(BOOK_TEST_ID);
         starWars.setTitle(BOOK_TEST_TITLE);
         starWars.setPrice(BOOK_TEST_PRICE);
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
         bookDao.create(starWars);
         Optional<Book> bookFounded = bookDao.findById(starWars.getId());
-        System.out.println("Was added : " + (bookFounded.isPresent() ? bookFounded.get() : "No"));
+        System.out.println("Added: " + (bookFounded.isPresent() ? bookFounded.get() : "No"));
         starWars.setPrice(BOOK_TEST_PRICE_NEW);
         bookDao.update(starWars);
         bookFounded = bookDao.findById(starWars.getId());
-        System.out.println("Was updated : " + (bookFounded.isPresent() ? bookFounded.get() : "No"));
-        System.out.println("Was deleted : " + starWars + " " + bookDao.deleteById(starWars.getId()));
+        System.out.println("Updated: " + (bookFounded.isPresent() ? bookFounded.get() : "No"));
+        System.out.println("Deleted: " + starWars + " " + bookDao.deleteById(starWars.getId()));
         List<Book> listBooks = bookDao.findAll();
         if (listBooks.size() > 0) {
             System.out.println("Now have in table books:");
