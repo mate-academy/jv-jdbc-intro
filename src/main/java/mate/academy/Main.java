e1cb2a339425c29ef4166afe6225ca85e44c2259
Kovaev <eugenkoval1010@gmail.com>
2023-12-19 16:02:13 +0200

    tasks solved

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,40 @@
 package mate.academy;
 
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
     public static void main(String[] args) {
+        File createTableScript = new File("src/main/resources/init_db.sql");
+        ScriptCreateTable scriptCreateTable = new ScriptCreateTable();
+        String sqlCreateTable = scriptCreateTable.loadSqlScriptFromFile(createTableScript);
+        scriptCreateTable.executeCreateTableQuery(sqlCreateTable);
+
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book footprintsOnTheRoad = new Book("Сліди на дорозі", BigDecimal.valueOf(500));
+        Book cleanCode = new Book("Clean Code", BigDecimal.valueOf(700));
+        Book effectiveJava = new Book("Effective Java", BigDecimal.valueOf(900));
+        bookDao.create(footprintsOnTheRoad);
+        bookDao.create(cleanCode);
+        bookDao.create(effectiveJava);
+
+        System.out.println(bookDao.findById(cleanCode.getId()));
+
+        cleanCode.setPrice(BigDecimal.valueOf(800));
+        bookDao.update(cleanCode);
+        System.out.println(bookDao.findById(cleanCode.getId()));
+
+        System.out.println(bookDao.findAll());
+
+        System.out.println(bookDao.deleteById(effectiveJava.getId()));
 
     }
 }
---
+++ src/main/java/mate/academy/ScriptCreateTable.java
@@ -0,0 +1,35 @@
+package mate.academy;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ScriptCreateTable {
+    public String loadSqlScriptFromFile(File scriptFile) {
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(scriptFile));
+            String value = reader.readLine();
+            while (value != null) {
+                stringBuilder.append(value).append(System.lineSeparator());
+                value = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading SQL script file: " + scriptFile.getAbsolutePath(), e);
+        }
+        return stringBuilder.toString();
+    }
+
+    public void executeCreateTableQuery(String sqlCreateTable) {
+        try (Connection connection = ConnectionUtil.getConnection();
+             Statement statement = connection.createStatement()){
+            statement.executeUpdate(sqlCreateTable);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing CREATE TABLE query: " + sqlCreateTable, e);
+        }
+    }
+}
