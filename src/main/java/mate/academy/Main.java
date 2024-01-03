dfc37c789e15847756c514a002dfe29874c0cab5
LeonIllya <dd0733567@gmail.com>
2024-01-03 14:05:00 +0200

    my decision

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,31 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector =
+            Injector.getInstance("mate.academy");
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book1 = bookDao.create(new Book("Harry Potter and the Philosopher's Stone",
+            new BigDecimal("10.45")));
+        Book book2 = bookDao.create(new Book("Harry Potter And The Chamber of secrets",
+            new BigDecimal("17.22")));
+        Book book3 = bookDao.create(new Book("Harry Potter and the prisoner of Azkaban",
+            new BigDecimal("20.15")));
+
+        book3.setPrice(new BigDecimal("10.56"));
+        System.out.println(bookDao.update(book3));
+
+        System.out.println();
+        System.out.println(bookDao.findById(1L));
+
+        bookDao.deleteById(2L);
+        System.out.println();
+
+        bookDao.findAll().forEach(System.out::println);
     }
 }
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `jv_jdbc_intro`.`books` (
+    `id` INT(10) NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `price` DECIMAL NOT NULL,
+    PRIMARY KEY (`id`)
+);
