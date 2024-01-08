e1be8024d0766f62ef924f6f5c3e0b82aea06553
Andrii Laver <laver.andriy@gmail.com>
2024-01-08 17:14:00 +0200

    created Book model, BookDao interface and it`s implementation

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,37 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Book firstBook = new Book();
+        Book secondBook = new Book();
+        firstBook.setTitle("The Adventures of Tom");
+        firstBook.setPrice(BigDecimal.valueOf(300));
+        secondBook.setTitle("Kobzar");
+        secondBook.setPrice(BigDecimal.valueOf(400));
+
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        bookDao.create(firstBook);
+        bookDao.create(secondBook);
+
+        Optional<Book> bookById = bookDao.findById(2L);
+        System.out.println(bookById + "\n");
+        List<Book> allBooks = bookDao.findAll();
+        allBooks.forEach(System.out::println);
+        System.out.println();
+
+        firstBook.setPrice(BigDecimal.valueOf(350));
+        bookDao.update(firstBook);
+        bookDao.deleteById(2L);
+        allBooks = bookDao.findAll();
+        allBooks.forEach(System.out::println);
     }
 }
