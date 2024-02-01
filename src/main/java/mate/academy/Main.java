b2280ff29e1dbd3e95380de28bf53be23bf1d098
Chorniypistolet <volkivskiy95@gmail.com>
2024-02-01 21:39:46 +0200

    hw jdbc-intro

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
+
 public class Main {
+
     public static void main(String[] args) {
+        Injector injector = Injector.getInstance("mate.academy");
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        bookDao.create(book);
+        bookDao.update(book);
+        bookDao.findAll();
+        Long id = book.getId();
+        bookDao.deleteById(id);
+        bookDao.findById(id);
 
     }
 }
