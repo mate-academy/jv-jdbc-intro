bc9fd13711f9eba080b16e6f47133c0dd9dd2c3a
AdamMudrak <mudrets2000@gmail.com>
2024-02-06 14:20:15 +0200

    Implement Dao

--- src/main/java/mate/academy/dao/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -1,34 +1,123 @@
 package mate.academy.dao;
 
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import mate.academy.lib.DaoImpl;
 import mate.academy.model.Book;
+import mate.academy.util.ConnectionUtil;
 
 @DaoImpl
 public class BookDaoImpl implements Dao<Book> {
+    private static final String CREATE_SQL = "INSERT INTO books (title, price) VALUES (?, ?)";
+    private static final String FIND_BY_ID_SQL = "SELECT * FROM books WHERE id = ?";
+    private static final String FIND_ALL_SQL = "SELECT * FROM books";
+    private static final String UPDATE_SQL = "UPDATE books SET title = ?,  price = ? WHERE id = ?";
+    private static final String DELETE_SQL = "DELETE books WHERE id = ?";
+
     @Override
     public Book create(Book book) {
-        return null;
+        try (Connection connection = ConnectionUtil.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(CREATE_SQL,
+                        Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, book.getTitle());
+            statement.setBigDecimal(2, book.getPrice());
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows < 1) {
+                throw new RuntimeException(
+                        "Expected to insert at least one row, " + "but inserted 0 rows");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            while (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                book.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't add a new book " + book, e);
+        }
+        return book;
     }
 
     @Override
     public Optional<Book> findById(Long id) {
-        return Optional.empty();
+        try (Connection connection = ConnectionUtil.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                String title = resultSet.getString("title");
+                BigDecimal price = resultSet.getBigDecimal("price");
+                return Optional.of(new Book(id, title, price));
+            } else {
+                return Optional.empty();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't establish connection with the DB", e);
+        }
     }
 
     @Override
     public List<Book> findAll() {
-        return null;
+        try (Connection connection = ConnectionUtil.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(FIND_ALL_SQL)) {
+            List<Book> booksList = new ArrayList<>();
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String title = resultSet.getString("title");
+                BigDecimal price = resultSet.getBigDecimal("price");
+                booksList.add(new Book(id, title, price));
+            }
+            if (booksList.isEmpty()) {
+                throw new RuntimeException(
+                        "The DB table \"books\" is empty. Add some elements first...");
+            } else {
+                return booksList;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't establish connection with the DB", e);
+        }
     }
 
     @Override
     public Book update(Book book) {
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(UPDATE_SQL)) {
+            statement.setString(1, book.getTitle());
+            statement.setBigDecimal(2, book.getPrice());
+            statement.setLong(3, book.getId());
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                Long id = resultSet.getLong("id");
+                String title = resultSet.getString("title");
+                BigDecimal price = resultSet.getBigDecimal("price");
+                return new Book(id, title, price);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
     public boolean deleteById(Long id) {
-        return false;
+        int updatedRows;
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(DELETE_SQL)) {
+            statement.setLong(1, id);
+            updatedRows = statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return updatedRows > 0;
     }
 }
