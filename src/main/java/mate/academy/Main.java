1321ffbcd820d58dfd2f67b0089a7f4bc209be08
Yana Stepanova <yanna.stepanova@gmail.com>
2024-03-07 00:22:42 +0200

    added solution to the task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,26 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book bookCreated = bookDao.create(new Book("The Chemist", BigDecimal.valueOf(399)));
+        System.out.println("Successful created book: " + bookCreated);
+        System.out.println(bookDao.create(new Book("The Host", BigDecimal.valueOf(129))));
+        System.out.println("Find by id: " + bookDao.findById(2L));
+        List<Book> allBooks = bookDao.findAll();
+        System.out.println("All books: " + allBooks);
+        System.out.println("Updated book: "
+                + bookDao.update(new Book(2L, "Renegades", BigDecimal.valueOf(500))));
+        System.out.println("Book with id = " + bookCreated.getId() + " was successful deleted: "
+                + bookDao.deleteById(bookCreated.getId()));
+        System.out.println("After deleted: " + bookDao.findAll());
     }
 }
