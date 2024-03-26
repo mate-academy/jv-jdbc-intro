ed1f2c3daaa9666c55306aceffb1c0e67d22873e
moskalenkosergii <moskalenko.s.work@gmail.com>
2024-03-26 12:45:55 +0100

    Created jdbc Dao

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,31 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book book = new Book();
+        book.setTitle("createBook");
+        book.setPrice(new BigDecimal(45));
+        bookDao.create(book);
+
+        bookDao.findById(1L);
+
+        List<Book> bookDaoAll = bookDao.findAll();
+
+        Book bookUpd = bookDaoAll.get(0);
+        bookUpd.setTitle("updateBookDao");
+        bookUpd.setPrice(new BigDecimal(300));
+        bookDao.update(bookUpd);
+
+        bookDao.deleteById(1L);
     }
 }
