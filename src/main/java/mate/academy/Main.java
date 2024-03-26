04a5126d46585e9756f353105f0846265eb49880
Starzhevskii <fkordprivate@gmail.com>
2024-03-26 15:17:10 +0200

    Implemented methods for Dao

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,38 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book kobzar = new Book("Kobzar", new BigDecimal("199.99"));
+        Book theWitcher = new Book("The Witcher", new BigDecimal("259.99"));
+
+        //add books to the database
+        bookDao.create(kobzar);
+        bookDao.create(theWitcher);
+
+        //check whether the books have been added
+        System.out.println(bookDao.findAll());
+
+        Book fandorin = new Book(1L,"Fandorin", new BigDecimal("150.55"));
+
+        //replace the Kobzar book with a new one
+        bookDao.update(fandorin);
+
+        //checked whether the book has been updated
+        System.out.println(bookDao.findById(1L));
+
+        //delete the book "The Witcher"
+        bookDao.deleteById(2L);
+
+        //check if all methods worked. Only the book "Fandorin" should remain
+        System.out.println(bookDao.findAll());
     }
 }
