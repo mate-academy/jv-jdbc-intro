20e12c4d18ee2fb305b5713c087e164aa197da88
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-27 12:40:09 +0200

    Main.java

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,19 +1,7 @@
 package mate.academy;
 
-import mate.academy.dao.BookDao;
-import mate.academy.lib.Injector;
-import mate.academy.model.Book;
-
-import java.math.BigDecimal;
-
 public class Main {
-    private static final Injector injector = Injector.getInstance("mate.academy");
-
     public static void main(String[] args) {
-        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
-        Book book = new Book();
-        book.setTitle("Sample Book");
-        book.setPrice(BigDecimal.valueOf(9.99));
-        bookDao.create(book);
+
     }
 }
