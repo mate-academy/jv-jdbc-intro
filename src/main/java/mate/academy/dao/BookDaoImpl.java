45512618ffdb25ba851aced8b274a950b67a9abf
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-03-28 00:45:52 +0200

    Connection created Script for creating table created + db creation book model created

---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,35 @@
+package mate.academy.dao;
+
+import mate.academy.lib.Dao;
+import mate.academy.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+@Dao
+public class BookDaoImpl implements BookDao{
+    @Override
+    public Book create(Book book) {
+        return null;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return null;
+    }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
+}
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE bookstore;
+USE bookstore;
 CREATE TABLE books (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
