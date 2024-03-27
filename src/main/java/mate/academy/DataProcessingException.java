f0d67f31c29127615d148faa46b101b64da9fa7e
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-03-27 12:39:19 +0200

    Delete src/main/java/mate/academy/DataProcessingException.java

--- src/main/java/mate/academy/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.exception;
-
-public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message, Throwable ex) {
-        super(message, ex);
-    }
-}
