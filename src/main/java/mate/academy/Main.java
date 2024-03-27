587bc6f9393c1997275790171fad998bb8ecbbe8
makaroshyna <katya23225@gmail.com>
2024-03-27 18:18:37 +0200

    Rename number of books -> books to create.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -9,14 +9,14 @@ import mate.academy.model.Book;
 public class Main {
     public static final Injector injector = Injector.getInstance("mate.academy.dao");
     private static final int BOOK_PRICE = 200;
-    private static final int NUMBER_OF_BOOKS = 3;
+    private static final int BOOKS_TO_CREATE = 3;
     private static final int NUMBER_OF_DELETED_BOOKS = 5;
 
     public static void main(String[] args) {
         BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
 
         Book book = new Book();
-        for (int i = 1; i <= NUMBER_OF_BOOKS; i++) {
+        for (int i = 1; i <= BOOKS_TO_CREATE; i++) {
             book.setTitle("Death Note. Book " + i);
             book.setPrice(BigDecimal.valueOf(BOOK_PRICE + i));
             bookDao.create(book);
