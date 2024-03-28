0f8c527c2b58249b386cbbf6c84a4c7b5417ff45
Chopyk Mykola <record7654@gmail.com>
2024-03-28 17:18:08 +0200

    Created intro task.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.models.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book(null, "Marry", 100);
+        // initialize field values using setters or constructor
+        bookDao.create(book);
+        System.out.println(bookDao.deleteById(20L));
+        System.out.println(bookDao.findAll());
+        System.out.println(bookDao.update(new Book(1L, "Anton", 33)));
+        System.out.println(bookDao.findById(1L));
     }
 }
