de6500fd9b74ebaa8cbcd46598384327c56eadb3
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-03-28 17:51:35 +0200

    New commit

---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `book` (
+                        `id` INT NOT NULL AUTO_INCREMENT,
+                        `title` VARCHAR(255),
+                        `price` DECIMAL(10,2),
+                        PRIMARY KEY (`id`)
+);
