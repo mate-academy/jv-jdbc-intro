197a6d7fca5863d708f2fbff5a9a7fa03a18bb87
Nazar Klimovych <rksvjdbx@gmail.com>
2024-03-28 10:27:30 +0200

    minor update

--- src/main/java/mate/academy/ConnectionUtil.java
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -9,7 +9,7 @@ public class ConnectionUtil {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/books";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Edizione123!";
+    private static final String PASSWORD = "password";
     private static final Properties DB_PROPERTIES;
 
     static {
