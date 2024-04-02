fe6a20e8e29f4597cf86d4377c5d350ca7a4f921
Denys <Eidenn005@gmail.com>
2024-04-02 14:27:19 +0300

    JDBC-intro Add needed solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,31 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.DAO.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    public static final Injector injector = Injector.getInstance("mate.academy.DAO");
+    public static final String BOOK_NAME = "Dune";
+    public static final int PRICE = 100;
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book dune = new Book(BOOK_NAME, BigDecimal.valueOf(PRICE));
+
+        bookDao.create(dune);
+        System.out.println(bookDao.findAll());
+
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("Dune2");
+        book.setPrice(BigDecimal.valueOf(500));
+
+        bookDao.update(book);
+        System.out.println(bookDao.findAll());
+
+        bookDao.deleteById(1L);
+        System.out.println(bookDao.findAll());
     }
 }
---
+++ src/main/java/mate/academy/Util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package mate.academy.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/book_schema";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", "root");
+        DB_PROPERTIES.put("password", "Qwerty");
+
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can not load JDBC driver: " + DRIVER, e);
+        }
+    }
+
+    public static Connection getconnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_PROPERTIES);
+    }
+
+}
