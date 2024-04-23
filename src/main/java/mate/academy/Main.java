f5f5b42864ea793aeaf769153859d2eff2e6c217
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-04-24 01:45:08 +0300

    finished task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,10 +1,9 @@
 package mate.academy;
 
 import java.math.BigDecimal;
-import mate.academy.lib.Injector;
 import mate.academy.dao.BookDao;
 import mate.academy.model.Book;
-
+import mate.academy.lib.Injector;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
