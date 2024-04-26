0604d2a1af5bdda388931de070dc10fba8efa2f4
Zorii Bohdan <bodkazorii@gmail.com>
2024-04-26 11:21:27 +0300

    changed package for injector

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -6,7 +6,7 @@ import mate.academy.lib.Injector;
 import mate.academy.model.Book;
 
 public class Main {
-    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
         BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
