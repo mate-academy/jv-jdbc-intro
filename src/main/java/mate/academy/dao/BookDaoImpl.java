a3d6d8a1c7e59c52b6a0aad55bc630f21d841f27
Vladlen Soloviov <retkinf@gmail.com>
2024-04-28 14:07:21 +0300

    do HW

--- src/main/java/mate/academy/dao/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -9,7 +9,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import mate.academy.ConnectionUtil;
 import mate.academy.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Book;
