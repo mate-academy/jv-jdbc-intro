3b22a03c646991e89d4dbfc1430fdcb6c1eeec7c
Bob <kostiantyn.ibraiev@gmail.com>
2024-05-05 15:01:37 +0200

    bookDao methods implemented and tested

---
+++ src/main/java/mate/academy/lib/service/ConnectionUtil.java
@@ -0,0 +1,30 @@
+package mate.academy.lib.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mate";
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "MySQL1914#";
+    private static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", DB_USERNAME);
+        DB_PROPERTIES.put("password", DB_PASSWORD);
+
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver : " + DB_DRIVER, e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_PROPERTIES);
+    }
+}
