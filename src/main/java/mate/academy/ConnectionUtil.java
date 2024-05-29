4c0def1193feade60e5de62c81467371bdbf2e55
kvjohnny <kvjohnny.van@gmail.com>
2024-05-29 16:46:25 +0300

    created, fixed classes and files in jv-jdbc-intro task

---
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -0,0 +1,26 @@
+package mate.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String DB_PATH = "jdbc:mysql://localhost:3306/intro";
+    private static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", "root");
+        DB_PROPERTIES.put("password", "root1234");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_PATH, DB_PROPERTIES);
+    }
+}
