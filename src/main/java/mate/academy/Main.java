e3f3c744b6088e39bf302f7f930883ed867a2692
Greshch <greshchook@gmail.com>
2024-07-04 08:36:02 +0300

    dao

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,6 +1,10 @@
 package mate.academy;
 
+import mate.academy.lib.Injector;
+
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
 
     }
---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,35 @@
+package mate.academy.dao;
+
+import mate.academy.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+import mate.academy.lib.Injector;
+
+@Dao
+public class BookDaoImpl implements BookDao {
+    @Override
+    public Book create(Book book) {
+        return null;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
+}
