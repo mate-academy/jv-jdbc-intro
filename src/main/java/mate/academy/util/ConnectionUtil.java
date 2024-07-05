3f60b502f5aa8dcd1ee1b063d70333b56818c9ee
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-07-05 10:10:11 +0300

    ConnectionUtil.java

---
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -0,0 +1,27 @@
+package mate.academy.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/your_schema?serverTimezone=UTC";
+    private static final String USER = "your_username";
+    private static final String PASSWORD = "your_password";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load MySQL driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't establish the connection to the database", e);
+        }
+    }
+}
