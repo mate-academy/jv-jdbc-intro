a80408b966644ad78429b053811276ced57264f7
Igor Glebov <igor.glebov0312@gmail.com>
2024-07-26 15:32:39 +0300

    Added changes after review;

--- src/main/java/mate/academy/util/ConnectionUtil.java
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -12,7 +12,7 @@ public class ConnectionUtil {
     static {
         DB_PROPERTIES = new Properties();
         DB_PROPERTIES.put("user", "root");
-        DB_PROPERTIES.put("password", "Nsty1601#");
+        DB_PROPERTIES.put("password", "rootroot");
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
