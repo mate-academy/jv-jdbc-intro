7945a51f037d43ed54ec83c77c47b01d3e62959d
Nikita Lafinskiy <niklafinskiy@gmail.com>
2024-09-03 23:21:51 +0200

    implemented the book storage system with jdbc

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,27 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        List<Book> books = List.of(new Book("MyBook1", BigDecimal.valueOf(60)),
+                new Book("MyBook2", BigDecimal.valueOf(14)),
+                new Book("MyBook3", BigDecimal.valueOf(51)));
+        for (Book book : books) {
+            bookDao.create(book);
+        }
+        bookDao.findById(1L);
+        bookDao.findAll();
+        Book book = new Book("MyBook4Update", BigDecimal.valueOf(19));
+        book.setId(1L);
+        bookDao.update(book);
+        bookDao.deleteById(1L);
     }
 }
