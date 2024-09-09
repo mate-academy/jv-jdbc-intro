74e7a1c4e1e156f69f727f310a4faebfd19c3d51
Nikolai <colya.reznick@gmail.com>
2024-09-09 18:52:02 +0300

    Added task solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,27 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("Java Programming");
+        book.setPrice(new BigDecimal(200));
+
+        bookDao.create(book);
+        bookDao.findAll().forEach(System.out::println);
+        System.out.println();
+        System.out.println("Id 1 " + System.lineSeparator() + bookDao.findById(1L));
+        bookDao.deleteById(2L);
+        bookDao.findAll().forEach(System.out::println);
+        System.out.println();
+        bookDao.update(book);
+        bookDao.findAll().forEach(System.out::println);
     }
 }
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS BookStore DEFAULT CHARACTER SET utf8;
+
+USE BookStore;
+
+CREATE TABLE Books (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+INSERT INTO Books (title, price) VALUES ('The Great Gatsby', 10.99);
+INSERT INTO Books (title, price) VALUES ('1984', 8.99);
+INSERT INTO Books (title, price) VALUES ('To Kill a Mockingbird', 12.50);
+INSERT INTO Books (title, price) VALUES ('Pride and Prejudice', 9.75);
+INSERT INTO Books (title, price) VALUES ('The Catcher in the Rye', 11.20);
