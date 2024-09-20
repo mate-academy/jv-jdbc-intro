e0c219d2f0038d4f093b83a681050c779ff5ab3f
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-21 00:38:28 +0300

    Implemented findAll

--- src/main/java/mate/academy/bookDao/BookDaoImpl.java
+++ src/main/java/mate/academy/bookDao/BookDaoImpl.java
@@ -1,4 +1,54 @@
 package mate.academy.bookDao;
 
-public class BookDaoImpl {
+import mate.academy.dbconnection.ConnectionUtil;
+import mate.academy.model.Book;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class BookDaoImpl implements BookDao {
+
+    @Override
+    public Book create(Book book) {
+        return null;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        List<Book> books = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM book")) {
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String title = resultSet.getString("tittle");
+                int price = resultSet.getInt("price");
+                Book book = new Book(id, title, price);
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't get all books from database", e);
+        }
+        return books;
+    }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
 }
