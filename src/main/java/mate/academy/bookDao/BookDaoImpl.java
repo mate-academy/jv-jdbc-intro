39326caaa5f88847aec51ad3786dbb11c7ad7d3c
Denys Mazurenko <0gur4ik3992@gmail.com>
2024-09-22 00:37:32 +0300

    used a static fabric method instead of a setter + constructor

--- src/main/java/mate/academy/bookDao/BookDaoImpl.java
+++ src/main/java/mate/academy/bookDao/BookDaoImpl.java
@@ -2,9 +2,11 @@ package mate.academy.bookDao;
 
 import mate.academy.dbconnection.ConnectionUtil;
 import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.Book;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,29 +14,62 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Dao
 public class BookDaoImpl implements BookDao {
 
     @Override
     public Book create(Book book) {
-        return null;
+        String sql = "INSERT INTO book(title, price) VALUES(?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setObject(1, book.getTitle());
+            statement.setInt(2, book.getPrice());
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows < 1) {
+                throw new RuntimeException("Book could not be created");
+            }
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                long id = generatedKeys.getLong(1);
+                book.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't add a new book " + book.getTitle(), e);
+        }
+        return book;
     }
 
     @Override
     public Optional<Book> findById(Long id) {
+        String sql = "SELECT * FROM book WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String title = resultSet.getString("title");
+                int price = resultSet.getInt("price");
+                Book book = Book.of(id, title, price);
+                return Optional.of(book);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't find book by id " + id + " ", e);
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Book> findAll() {
+        String sql = "SELECT * FROM book";
         List<Book> books = new ArrayList<>();
         try (Connection connection = ConnectionUtil.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery("SELECT * FROM book")) {
+             ResultSet resultSet = statement.executeQuery(sql)) {
             while (resultSet.next()) {
                 long id = resultSet.getLong("id");
-                String title = resultSet.getString("tittle");
+                String title = resultSet.getString("title");
                 int price = resultSet.getInt("price");
-                Book book = new Book(id, title, price);
+                Book book = Book.of(id, title, price);
                 books.add(book);
             }
         } catch (SQLException e) {
@@ -45,6 +80,7 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public Book update(Book book) {
+        String sql = "UPDATE book SET title = ?, price = ? WHERE id = ?";
         return null;
     }
 
