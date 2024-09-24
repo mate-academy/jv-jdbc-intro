5c4099c84ee1dfc4a92fc34131497246774b83f9
trokhim03 <trokhimchuk.003@gmail.com>
2024-09-24 16:47:21 +0300

    solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,31 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        Book firstBook = new Book(1L, "Whispers of the Forgotten Forest",
+                BigDecimal.valueOf(300.00));
+        Book secondBook = new Book(2L, "The Last Light of Dusk", BigDecimal.valueOf(450.00));
+        Book thirdBook = new Book(3L, "Echoes of Time", BigDecimal.valueOf(500.00));
+        Book updateFirstBook = new Book(1L, "Whispers of the Forgotten Forest",
+                BigDecimal.valueOf(700.00));
+
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        System.out.println("Book created: " + bookDao.create(firstBook));
+        System.out.println("Book created: " + bookDao.create(secondBook));
+        System.out.println("Book created: " + bookDao.create(thirdBook));
+        System.out.println("Book updated: " + bookDao.update(updateFirstBook));
+        System.out.println("Book found by ID (" + secondBook.getId() + "): "
+                + bookDao.findById(secondBook.getId()));
+        System.out.println("All books in the library: " + bookDao.findAll());
+        System.out.println("Book deleted by ID (" + thirdBook.getId()
+                + "): " + bookDao.deleteById(thirdBook.getId()));
     }
 }
---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,146 @@
+package mate.academy.dao;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Book;
+import mate.academy.util.ConnectionUtil;
+
+@Dao
+public class BookDaoImpl implements BookDao {
+    private static final int FIRST_PARAMETER_INDEX = 1;
+    private static final int SECOND_PARAMETER_INDEX = 2;
+    private static final int THIRD_PARAMETER_INDEX = 3;
+    private static final int FIRST_COLUMN_INDEX = 1;
+    private static final int SECOND_COLUMN_INDEX = 2;
+    private static final int THIRD_COLUMN_INDEX = 3;
+    private static final String SQL_CREATE_REQUEST = "INSERT INTO "
+            + "books (title, price) VALUES (?, ?)";
+    private static final String SQL_UPDATE_REQUEST = "UPDATE books"
+            + " SET title = ?, price = ? WHERE id = ?";
+    private static final String SQL_DELETE_REQUEST = "DELETE FROM"
+            + " books WHERE id = ?";
+    private static final String SQL_OUTPUT_REQUEST = "SELECT * "
+            + "FROM books";
+    private static final String SQL_BY_ID_REQUEST = "SELECT * "
+            + "FROM books WHERE id = ?";
+
+    @Override
+    public Book create(Book book) {
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement = connection
+                           .prepareStatement(SQL_CREATE_REQUEST, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(FIRST_PARAMETER_INDEX, book.getTitle());
+            preparedStatement.setBigDecimal(SECOND_PARAMETER_INDEX, book.getPrice());
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows < 1) {
+                throw new DataProcessingException("Expected to "
+                        + "inserted at least one row, but inserted 0 rows.");
+            }
+
+            ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(FIRST_COLUMN_INDEX, Long.class);
+                book.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Failed to establish connection"
+                    + " with the database", e);
+        }
+        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        Optional<Book> optionalBook = Optional.empty();
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement = connection
+                           .prepareStatement(SQL_BY_ID_REQUEST)) {
+            preparedStatement.setLong(FIRST_PARAMETER_INDEX, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Long identify = resultSet.getLong(FIRST_COLUMN_INDEX);
+                String title = resultSet.getString(SECOND_COLUMN_INDEX);
+                BigDecimal price = resultSet.getBigDecimal(THIRD_COLUMN_INDEX);
+
+                Book book = new Book(identify, title, price);
+                optionalBook = Optional.of(book);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Failed to establish connection"
+                    + " with the database", e);
+        }
+        return optionalBook;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        List<Book> bookList = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement = connection
+                        .prepareStatement(SQL_OUTPUT_REQUEST)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                Long id = resultSet.getLong(FIRST_COLUMN_INDEX);
+                String title = resultSet.getString(SECOND_COLUMN_INDEX);
+                BigDecimal price = resultSet.getBigDecimal(THIRD_COLUMN_INDEX);
+
+                Book book = new Book(id, title, price);
+                bookList.add(book);
+            }
+
+        } catch (SQLException e) {
+            throw new DataProcessingException("Failed to establish connection"
+                    + " with the database", e);
+        }
+        return bookList;
+    }
+
+    @Override
+    public Book update(Book book) {
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement = connection
+                        .prepareStatement(SQL_UPDATE_REQUEST)) {
+            preparedStatement.setString(FIRST_PARAMETER_INDEX, book.getTitle());
+            preparedStatement.setBigDecimal(SECOND_PARAMETER_INDEX, book.getPrice());
+            preparedStatement.setLong(THIRD_PARAMETER_INDEX, book.getId());
+
+            int affectedRows = preparedStatement.executeUpdate();
+
+            if (affectedRows < 1) {
+                throw new DataProcessingException("No book found with ID: " + book.getId());
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Failed to establish connection"
+                    + " with the database", e);
+        }
+        return book;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        int affectedRows = 0;
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement preparedStatement = connection
+                        .prepareStatement(SQL_DELETE_REQUEST)) {
+            preparedStatement.setLong(FIRST_PARAMETER_INDEX, id);
+            affectedRows = preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataProcessingException("Failed to establish "
+                    + "connection with the database", e);
+        }
+        return affectedRows > 0;
+    }
+}
