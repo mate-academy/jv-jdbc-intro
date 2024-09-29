fe4f09a216d092ecea95abc60f557d144f839045
Nikita Khilobok <nicktich284@gmail.com>
2024-09-29 20:04:40 +0300

    hw is done, please check

---
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package mate.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/jdbc_intro";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Nk102030!";
+    private static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", USERNAME);
+        DB_PROPERTIES.put("password", PASSWORD);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can not load JDBC driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_PROPERTIES);
+    }
+}
---
+++ src/main/java/mate/academy/service/DataProcessingException.java
@@ -0,0 +1,7 @@
+package mate.academy.service;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message) {
+        super(message);
+    }
+}
