c039e8d7cb1524323fd156682bf97a2803b36e66
Oleksandr Prots <sashaproc2289@gmail.com>
2024-10-02 21:55:40 +0300

    jv-jdbc-intro

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,50 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) INJECTOR.getInstance(BookDao.class);
+
+        Optional<Book> foundBook = bookDao.findById(1L);
+        if (foundBook.isPresent()) {
+            System.out.println("Book found: " + foundBook.get());
+        } else {
+            System.out.println("Book not found");
+        }
+        Book book = new Book();
+        book.setId(1L);
+        book.setPrice(new BigDecimal("100.00"));
+        book.setTitle("Alice");
+        bookDao.create(book);
+        System.out.println("Book created: " + book);
+
+        if (foundBook.isPresent()) {
+            Book bookToUpdate = foundBook.get();
+            bookToUpdate.setTitle("Alice in Wonderland");
+            bookToUpdate.setPrice(new BigDecimal("120.00"));
+            bookDao.update(bookToUpdate);
+            System.out.println("Book updated: " + bookToUpdate);
+        }
+
+        List<Book> allBooks = bookDao.findAll();
+        System.out.println("All books in the database:");
+        for (Book b : allBooks) {
+            System.out.println(b);
+        }
+
+        boolean isDeleted = bookDao.deleteById(1L);
+        if (isDeleted) {
+            System.out.println("Book with ID 1 deleted.");
+        } else {
+            System.out.println("Book with ID 1 not found.");
+        }
     }
 }
---
+++ src/main/java/mate/academy/lib/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package mate.academy.lib;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USER = "root";
+    private static final String PASSWORD = "qwerty123";
+    private static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", USER);
+        DB_PROPERTIES.put("password", PASSWORD);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can not load JDBC driver", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL);
+    }
+}
