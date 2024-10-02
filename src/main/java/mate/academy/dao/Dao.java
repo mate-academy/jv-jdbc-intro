6868da5b2fb3fa99b4a5004549e0399b7db543ac
Svitlana Galicka <svitlanakozak12332@gmail.com>
2024-10-02 22:11:18 +0300

    created a Book entity class and BookDao repository interface

--- src/main/java/mate/academy/dao/Dao.java
+++ src/main/java/mate/academy/dao/Dao.java
@@ -1,8 +1,9 @@
-package mate.academy.lib;
+package mate.academy.dao;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Dao {
+
 }
