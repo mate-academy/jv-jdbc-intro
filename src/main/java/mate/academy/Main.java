dbbdbd7b86836ae02dd1ffa148221d6a7be30481
Bartlomiej Smreczak <b.smreczak@gmail.com>
2024-10-27 18:18:37 +0100

    Second solution - fixes after code review: corrected CRUD methods, removed comments

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -13,21 +13,19 @@ public class Main {
     public static void main(String[] args) {
         BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
         List<Book> books = Arrays.asList(
-                new Book(1L, "Book1", BigDecimal.valueOf(5.99)),
-                new Book(2L, "Book2", BigDecimal.valueOf(4.35)),
-                new Book(3L, "Book3", BigDecimal.valueOf(10.00))
+                new Book(1L, "Catch-22", BigDecimal.valueOf(5.37)),
+                new Book(2L, "Winnie the Pooh", BigDecimal.valueOf(254.01)),
+                new Book(3L, "Harry Potter", BigDecimal.valueOf(90.00)),
+                new Book(4L, "Crime and Punishment", BigDecimal.valueOf(15.00))
         );
+
         books.forEach(bookDao::create);
         Book secondBook = books.get(1);
-        secondBook.setTitle("Book4");
+        secondBook.setTitle("No name");
         bookDao.update(secondBook);
         System.out.println(bookDao.findById(2L));
         System.out.println(bookDao.findAll());
         bookDao.deleteById(3L);
-
-        // initialize field values using setters or constructor
-        //bookDao.create(book);
-        // test other methods from BookDao
 
     }
 }
