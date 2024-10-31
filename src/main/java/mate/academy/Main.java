63a248426a85d64d4948ec2b82a583fe2000df80
rus1980707 <rus1980707@gmail.com>
2024-10-31 20:04:50 +0200

    jv-jdbc-intro

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,30 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("Effective Java");
+        book.setPrice(new BigDecimal("45.99"));
+
+        bookDao.create(book);
+        System.out.println("Created book: " + book);
+
+        Book fetchedBook = bookDao.findById(book.getId()).orElseThrow();
+        System.out.println("Fetched book: " + fetchedBook);
+
+        book.setPrice(new BigDecimal("50.99"));
+        bookDao.update(book);
+        System.out.println("Updated book: " + book);
+
+        bookDao.deleteById(book.getId());
+        System.out.println("Book deleted: " + book.getId());
     }
 }
