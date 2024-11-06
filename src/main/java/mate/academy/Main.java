1d9c588e6b95ae35c5b76339cf21b34fb5a5a9e9
StoneBlood <brkalix@gmail.com>
2024-11-06 20:28:21 +0200

    created the class ConnectionUtil

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,9 @@
 package mate.academy;
 
+import mate.academy.db.DatabaseInitializer;
+
 public class Main {
     public static void main(String[] args) {
-
+        DatabaseInitializer.initializeDatabaseScript();
     }
 }
