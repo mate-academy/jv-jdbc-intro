e223fd61861845117d3107cca800cc62bacc1d2c
StoneBlood <brkalix@gmail.com>
2024-11-06 20:31:39 +0200

    created the class Book

---
+++ src/main/java/mate/academy/services/Book.java
@@ -0,0 +1,37 @@
+package mate.academy.services;
+
+public class Book {
+    private Long id;
+    private String model;
+    private int price;
+
+    public Book(Long id, String model, int price) {
+        this.id = id;
+        this.model = model;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
