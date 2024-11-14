0dd58c89bec3a34c1ad272398c3f91164cd111ef
clo3eX <pansydaisy83@gmail.com>
2024-11-14 11:25:17 +0100

    added minor changes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -10,10 +10,10 @@ public class Main {
 
     public static void main(String[] args) {
         final BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
-        Book book1 = new Book("book1", BigDecimal.valueOf(1022));
-        Book book2 = new Book(4L,"book2", BigDecimal.valueOf(12));
-        bookDao.create(book1);
-        bookDao.update(book2);
+        Book bookOne = new Book("book one", BigDecimal.valueOf(1022));
+        Book bookTwo = new Book(4L,"book two", BigDecimal.valueOf(12));
+        bookDao.create(bookOne);
+        bookDao.update(bookTwo);
         System.out.println(bookDao.findById(1L));
         System.out.println(bookDao.findAll());
         bookDao.deleteById(1L);
--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -6,9 +6,9 @@ CREATE TABLE books(
 
 USE test;
 
-INSERT INTO books (title, price) VALUES ('book3', 10.00);
-INSERT INTO books (title, price) VALUES ('book4', 15.40);
-INSERT INTO books (title, price) VALUES ('book5', 25.00);
-INSERT INTO books (title, price) VALUES ('book67', 267.00);
-INSERT INTO books (title, price) VALUES ('book6', 159.50);
-INSERT INTO books (title, price) VALUES ('book8', 2444.99);
+INSERT INTO books (title, price) VALUES ('Book One', 10.00);
+INSERT INTO books (title, price) VALUES ('Book Two', 15.40);
+INSERT INTO books (title, price) VALUES ('Book Three', 25.00);
+INSERT INTO books (title, price) VALUES ('Book Four', 267.00);
+INSERT INTO books (title, price) VALUES ('Book Five', 159.50);
+INSERT INTO books (title, price) VALUES ('Book Six', 2444.99);
