608611d47afe4f0be19e158487d08d6dd0aecc35
Ihor Tkachuk <iono19031@gmail.com>
2024-11-19 21:52:49 +0200

    added solution

---
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -0,0 +1,32 @@
+package mate.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/books_database";
+    private static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", "root");
+        DB_PROPERTIES.put("password", "mateAc11");
+
+        try {
+            String className = "com.mysql.cj.jdbc.Driver";
+            Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Cannot load JDBC driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(DB_URL, DB_PROPERTIES);
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot create a connection to the DB", e);
+        }
+    }
+}
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,8 @@
+USE books_database;
+
+INSERT INTO books (title, price) VALUES ('The Great Gatsby', 19.99);
+INSERT INTO books (title, price) VALUES ('1984', 15.99);
+INSERT INTO books (title, price) VALUES ('To Kill a Mockingbird', 14.99);
+INSERT INTO books (title, price) VALUES ('Pride and Prejudice', 12.99);
+INSERT INTO books (title, price) VALUES ('Moby-Dick', 22.50);
+
