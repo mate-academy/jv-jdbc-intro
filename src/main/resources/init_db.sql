c09283d465dd9f24f22f47926e345f77db6f5841
Ihor Tkachuk <iono19031@gmail.com>
2024-11-19 22:33:30 +0200

    added init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -4,5 +4,4 @@ INSERT INTO books (title, price) VALUES ('The Great Gatsby', 19.99);
 INSERT INTO books (title, price) VALUES ('1984', 15.99);
 INSERT INTO books (title, price) VALUES ('To Kill a Mockingbird', 14.99);
 INSERT INTO books (title, price) VALUES ('Pride and Prejudice', 12.99);
-INSERT INTO books (title, price) VALUES ('Moby-Dick', 22.50);
 
