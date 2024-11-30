2acd94c826b103e1eb21eb421f9e82cfe563e946
d.nosenko <daniil.nosenkoo@gmail.com>
2024-11-30 16:46:45 +0100

    solution

---
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -0,0 +1,28 @@
+package mate.academy.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/bookshop";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "7045642NvL!";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load JDBC driver for MySQL", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't establish connection to the database", e);
+        }
+    }
+}
