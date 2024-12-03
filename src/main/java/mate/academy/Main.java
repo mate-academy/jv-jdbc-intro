c553d82bb117f43dffa6a07d9e7eda9954e4dad2
Artur <a_blyskal@wp.pl>
2024-12-03 18:44:17 +0100

    jv-jdbc-intro - first attempt

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,35 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector
+            = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book firstBook = new Book("Criminal", BigDecimal.valueOf(100));
+        Book secondBook = new Book("Good man", BigDecimal.valueOf(110));
+        Book savedBook = bookDao.create(firstBook);
+        bookDao.create(secondBook);
+        System.out.println("Book was added to database: " + savedBook);
+
+        Book updateBook = bookDao.findById(savedBook.getId()).orElseThrow(() ->
+                new DataProcessingException("Book was not found"));
+        updateBook.setPrice(BigDecimal.valueOf(150));
+        updateBook.setTitle("Updated Criminal");
+        bookDao.update(updateBook);
+
+        System.out.println("All books in the database:");
+        bookDao.findAll().forEach(System.out::println);
+
+        if (bookDao.deleteById(updateBook.getId())) {
+            System.out.println("Book was deleted");
+        }
     }
 }
