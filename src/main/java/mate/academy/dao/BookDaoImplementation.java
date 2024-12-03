ca8f5340ab83a9b4a4fac944a768112a17e2636b
Artur <a_blyskal@wp.pl>
2024-12-03 19:04:21 +0100

    used custom exception class

--- src/main/java/mate/academy/dao/BookDaoImplementation.java
+++ src/main/java/mate/academy/dao/BookDaoImplementation.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
 import mate.academy.model.Book;
