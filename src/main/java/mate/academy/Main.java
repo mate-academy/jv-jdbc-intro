f1a95fee59b7815ee3e94d6189f6b405cc17e6d9
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-12-04 04:35:09 +0200

    implement findById method

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,6 +1,7 @@
 package mate.academy;
 
 import mate.academy.dao.BookDao;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Injector;
 import mate.academy.model.Book;
 
@@ -16,13 +17,28 @@ public class Main {
         Book book2 = new Book("Some book 2", BigDecimal.valueOf(22.22));
         Book book3 = new Book("Some book 3", BigDecimal.valueOf(33.33));
 
-        book1 = bookDao.create(book1);
-        book2 = bookDao.create(book2);
-        book3 = bookDao.create(book3);
+        Book createdBook1 = bookDao.create(book1);
+        Book createdBook2 = bookDao.create(book2);
+        Book createdBook3 = bookDao.create(book3);
 
         System.out.println("------------= CREATE =------------");
-        System.out.println(book1 + " created!");
-        System.out.println(book2 + " created!");
-        System.out.println(book3 + " created!");
+        System.out.println(createdBook1 + " created!");
+        System.out.println(createdBook2 + " created!");
+        System.out.println(createdBook3 + " created!");
+        System.out.println();
+
+        System.out.println("------------= FIND BY ID =------------");
+        Long id1 = book1.getId();
+        Book foundBook1 = bookDao.findById(id1).orElseThrow(
+                () -> new DataProcessingException("Can't find book with id=" + id1));
+        System.out.println("Book with id=" + id1 + ": " + foundBook1);
+        Long id2 = book1.getId();
+        Book foundBook2 = bookDao.findById(id2).orElseThrow(
+                () -> new DataProcessingException("Can't find book with id=" + id2));
+        System.out.println("Book with id=" + id2 + ": " + foundBook2);
+        Long id3 = book1.getId();
+        Book foundBook3 = bookDao.findById(id3).orElseThrow(
+                () -> new DataProcessingException("Can't find book with id=" + id3));
+        System.out.println("Book with id=" + id3 + ": " + foundBook3);
     }
 }
--- src/main/java/mate/academy/dao/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -23,7 +23,6 @@ public class BookDaoImpl implements BookDao {
 
             statement.setString(1, book.getTitle());
             statement.setBigDecimal(2, book.getPrice());
-
             int affectedRows = statement.executeUpdate();
             if (affectedRows < 1) {
                 throw new DataProcessingException(" Can't create a new book");
@@ -36,11 +35,30 @@ public class BookDaoImpl implements BookDao {
         } catch (SQLException e) {
             throw new DataProcessingException("Can't create a new book", e);
         }
+
         return book;
     }
 
     @Override
     public Optional<Book> findById(Long id) {
+        String query = "SELECT * FROM books WHERE id=?";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Book book = new Book(
+                        resultSet.getNString("title"),
+                        resultSet.getBigDecimal("price"));
+                book.setId(resultSet.getLong("id"));
+
+                return Optional.of(book);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return Optional.empty();
     }
 
