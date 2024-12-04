6a7d464d6719b3eea476f40b8fc801efd40ca65b
Anatoly Olkhovsky <anatoltbcm@gmail.com>
2024-12-04 04:15:09 +0200

    implement create method

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,28 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+import java.math.BigDecimal;
+
+public class Main {
+    public static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book book1 = new Book("Some book 1", BigDecimal.valueOf(11.11));
+        Book book2 = new Book("Some book 2", BigDecimal.valueOf(22.22));
+        Book book3 = new Book("Some book 3", BigDecimal.valueOf(33.33));
+
+        book1 = bookDao.create(book1);
+        book2 = bookDao.create(book2);
+        book3 = bookDao.create(book3);
+
+        System.out.println("------------= CREATE =------------");
+        System.out.println(book1 + " created!");
+        System.out.println(book2 + " created!");
+        System.out.println(book3 + " created!");
     }
 }
---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,61 @@
+package mate.academy.dao;
+
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Book;
+import mate.academy.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Dao
+public class BookDaoImpl implements BookDao {
+    @Override
+    public Book create(Book book) {
+        String query = "INSERT INTO books (title, price) VALUES (?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement statement = connection
+                     .prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+            statement.setString(1, book.getTitle());
+            statement.setBigDecimal(2, book.getPrice());
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows < 1) {
+                throw new DataProcessingException(" Can't create a new book");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                book.setId(generatedKeys.getLong(1));
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't create a new book", e);
+        }
+        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
+}
