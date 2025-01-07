a6a52995c172d7f87a969f1ab1aee3980b5c4974
homeworkfirst <zookotelif@gmail.com>
2025-01-07 23:32:32 +0200

    Add: Implement Book entity, DAO layer with CRUD methods, custom exception handling, and database connectivity setup. Includes init_db.sql for table creation and main class for method testing.

--- src/main/java/mate/academy/lib/BookDaoImpl.java
+++ src/main/java/mate/academy/lib/BookDaoImpl.java
@@ -1,7 +1,12 @@
 package mate.academy.lib;
 
 import java.math.BigDecimal;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -16,7 +21,8 @@ public class BookDaoImpl implements BookDao {
     public Book create(Book book) {
         String query = "INSERT INTO books (title, price) VALUES (?, ?)";
         try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-             PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+             PreparedStatement statement = connection.prepareStatement(query,
+                     Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, book.getTitle());
             statement.setBigDecimal(2, book.getPrice());
             statement.executeUpdate();
