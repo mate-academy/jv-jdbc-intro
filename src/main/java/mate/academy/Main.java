7881ff6ed0d87f8f2904d28b0b637bddf0904164
oKukHtina <olakukhtina95@gmail.com>
2025-01-09 20:21:06 +0200

    created task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,7 +2,6 @@ package mate.academy;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 import mate.academy.dao.BookDao;
 import mate.academy.lib.Injector;
 import mate.academy.model.Book;
@@ -13,34 +12,27 @@ public class Main {
     public static void main(String[] args) {
         BookDao bookDao = (BookDao) INJECTOR.getInstance(BookDao.class);
 
-        Optional<Book> foundBook = bookDao.findById(1L);
-        if (foundBook.isPresent()) {
-            System.out.println("Book found:" + foundBook.get());
+        bookDao.findById(1L).ifPresentOrElse(book -> {
+            System.out.println("Book found: " + book);
 
-            Book bookToUpdate = foundBook.get();
-            bookToUpdate.setTitle("Alice in Wonderland");
-            bookToUpdate.setPrice(new BigDecimal("200.00"));
-            bookDao.update(bookToUpdate);
+            book.setTitle("Alice in Wonderland");
+            book.setPrice(new BigDecimal("200.00"));
+            bookDao.update(book);
 
-            System.out.println("Book updated: " + bookToUpdate);
-
-        } else {
-            Book book = new Book();
-            book.setPrice(new BigDecimal("100.00"));
-            book.setTitle("Alice");
-            bookDao.create(book);
-            System.out.println("Book created: " + book);
-        }
+            System.out.println("Book updated: " + book);
+        }, () -> {
+            Book newBook = new Book();
+            newBook.setPrice(new BigDecimal("100.00"));
+            newBook.setTitle("Alice");
+            bookDao.create(newBook);
+            System.out.println("Book created: " + newBook);
+        });
 
         List<Book> allBooks = bookDao.findAll();
         System.out.println("All books in the database:");
-
-        for (Book b : allBooks) {
-            System.out.println(b);
-        }
+        allBooks.forEach(System.out::println);
 
         boolean isDeleted = bookDao.deleteById(1L);
-
         if (isDeleted) {
             System.out.println("Book with ID 1 deleted.");
         } else {
