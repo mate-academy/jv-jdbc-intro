71d825c39903d2003b899d091387f3caf3c25da4
skviropitek <skvyra.anton@gmail.com>
2025-01-12 15:57:43 +0100

    solution created

--- src/main/java/mate/academy/ConnectionUtil.java
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -8,11 +8,13 @@ import java.util.Properties;
 public class ConnectionUtil {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/books";
     private static final Properties DB_PROPERTIES;
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "41456514aA";
 
     static {
         DB_PROPERTIES = new Properties();
-        DB_PROPERTIES.setProperty("user", "root");
-        DB_PROPERTIES.setProperty("password", "41456514aA");
+        DB_PROPERTIES.setProperty("user", DB_USER);
+        DB_PROPERTIES.setProperty("password", DB_PASSWORD);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
