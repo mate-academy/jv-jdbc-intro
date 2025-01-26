bb3c758d0caea4983446df3fc0de3dc18273f091
PawelCp <capiukpawel@gmail.com>
2025-01-26 12:11:45 +0100

    First approach to the jdbc intro task

---
+++ src/main/java/mate/academy/ConnectionUtil.java
@@ -0,0 +1,34 @@
+package mate.academy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil implements AutoCloseable {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/jdbcintro";
+    private static final Properties DB_PROPERTIES;
+
+    static {
+        DB_PROPERTIES = new Properties();
+        DB_PROPERTIES.put("user", "root");
+        DB_PROPERTIES.put("password", "Capi2001@");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load MSQL driver", e);
+        }
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_PROPERTIES);
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+}
