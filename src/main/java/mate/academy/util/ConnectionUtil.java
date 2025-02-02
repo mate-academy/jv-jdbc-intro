f5eea178f5487fc324a6f08e0575db3f15ad1ecd
Dmytro Tkachuk <dmitrotkacuk071@gmail.com>
2025-02-02 21:47:55 +0100

    Create the Book entity class and the BookDao repository interface, as well as its implementation.
    Establish a connection to your database.
    Create an init_db.sql file in the src/main/resources folder. In this file, place the scripts to create the necessary table.
    Create the Book model.
    Create a DAO layer for the Book model.

---
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -0,0 +1,32 @@
+package mate.academy.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/f_book_store";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "mysql666";
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    static {
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load jdbc driver for MySql", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            Properties dbProperties = new Properties();
+            dbProperties.put("user", USERNAME);
+            dbProperties.put("password", PASSWORD);
+            return DriverManager.getConnection(URL, dbProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB", e);
+        }
+    }
+}
