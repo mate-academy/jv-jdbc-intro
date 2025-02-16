05a663aff1772cecef562d01eddca15a78f69f72
eulu <anegamega@gmail.com>
2025-02-16 17:21:29 +0200

    Implement findById dao method

--- src/main/java/mate/academy/dao/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -2,7 +2,12 @@ package mate.academy.dao;
 
 import mate.academy.lib.Dao;
 import mate.academy.model.Book;
+import mate.academy.util.ConnectionUtil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +20,21 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public Optional<Book> findById(Long id) {
+        String sql = "select * from books where id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setObject(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                Book book = new Book();
+                book.setId(resultSet.getLong("id"));
+                book.setPrice(resultSet.getBigDecimal("price"));
+                book.setTitle(resultSet.getString("title"));
+                return Optional.of(book);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot find book with id = " + id, e);
+        }
         return Optional.empty();
     }
 
