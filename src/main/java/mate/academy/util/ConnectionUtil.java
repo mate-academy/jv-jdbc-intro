dcd8f125e54e786bef0ac3b85b75f3758eed9a02
Arina <crowlynx4@gmail.com>
2025-02-17 14:56:12 +0200

    change

--- src/main/java/mate/academy/util/ConnectionUtil.java
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -3,7 +3,6 @@ package mate.academy.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/db_test";
