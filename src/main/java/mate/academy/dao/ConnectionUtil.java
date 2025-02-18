b62bc5481f88ad9ba3691e34998280dbbc94522e
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-02-18 18:05:33 +0200

    solve import problems

--- src/main/java/mate/academy/dao/ConnectionUtil.java
+++ src/main/java/mate/academy/dao/ConnectionUtil.java
@@ -1,11 +1,8 @@
 package mate.academy.dao;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class ConnectionUtil {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/users";
