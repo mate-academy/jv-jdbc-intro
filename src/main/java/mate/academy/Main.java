6ead1455df94ac2b5de93add544eca1a99bd0916
Sergey <westard@proton.me>
2025-02-22 19:07:05 +0200

    task completed

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,31 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+import java.math.BigDecimal;
+
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        // initialize field values using setters or constructor
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("Book 1");
+        book.setPrice(BigDecimal.valueOf(33.33));
+
+        // test other methods from BookDao
+        System.out.println("Created book: " + bookDao.create(book));
+        System.out.println("All books: " + bookDao.findAll());
+        System.out.println("Book by ID 1: " + bookDao.findById(1L).orElse(null));
+        book.setTitle("Updated Book");
+        book.setPrice(BigDecimal.valueOf(44.44));
+        System.out.println("Updated book: " + bookDao.update(book));
+        System.out.println("Delete book with ID 1: " + bookDao.deleteByID(1L));
+        System.out.println("All books after deletion: " + bookDao.findAll());
     }
 }
---
+++ src/main/java/mate/academy/exceptions/DataProcessingException.java
@@ -0,0 +1,8 @@
+package mate.academy.exceptions;
+
+public class DataProcessingException extends RuntimeException {
+
+    public DataProcessingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
