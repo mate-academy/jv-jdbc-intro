f0b6911302a06273c0824a0283d65c81ee50e715
user <graban2253@gmail.com>
2025-02-26 15:37:06 +0200

    Finished

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,34 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+public class Main {
+    public static final Injector injector = Injector.getInstance("mate.academy.lib");
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book book = new Book();
+        book.setId(null);
+        book.setTitle("Java+");
+        book.setPrice(new BigDecimal("50.0"));
+        book = bookDao.create(book);
+
+        Optional<Book> findBook = bookDao.findById(book.getId());
+
+        List<Book> books = bookDao.findAll();
+
+
+        book.setTitle("Java++");
+        book.setPrice(new BigDecimal("70.0"));
+
+        book = bookDao.update(book);
+
+        boolean isDel = bookDao.deleteById(book.getId());
     }
 }
