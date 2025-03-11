cbd521ca2af7dec272f5509aab04fd436112b740
Solvain <maluginm72@gmail.com>
2025-03-11 20:57:29 +0200

    89

---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,47 @@
+package mate.academy.model;
+
+public class User {
+    private int id;
+    private final String name;
+    private final String email;
+    private final String password;
+
+    public User(int id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+//
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
