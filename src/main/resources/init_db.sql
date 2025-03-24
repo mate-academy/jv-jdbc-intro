7f4f71cb00a56c190536a43191be7002bc413d8f
Geks <gekatonheyr@ukr.net>
2025-03-24 18:28:27 +0200

    Had worked under DB: - created init_db.sql with initial set of comands; - created Book model; - created DAO layer for Book and implemented required methods; - created custom Exception to signal about emergency situations; - in the Main method tested all CRUD methods of DAO.
    findById method returns the Optional<Book> object.

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -3,10 +3,10 @@ USE hw_books;
 
 CREATE TABLE books (
                        id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                       title VARCHAR(255) NOT NULL ,
-                       price DECIMAL(16)
+                       title VARCHAR (255) NOT NULL ,
+                       price DECIMAL (16)
 );
 
-INSERT INTO books (title, price) VALUES('The Mythical Man-Month', 260);
-INSERT INTO books (title, price) VALUES('The Art of Computer Programming', 500);
-INSERT INTO books (title, price) VALUES('The Dogs of War', 180);
+INSERT INTO books (title, price) VALUES ('The Mythical Man-Month', 260);
+INSERT INTO books (title, price) VALUES ('The Art of Computer Programming', 500);
+INSERT INTO books (title, price) VALUES ('The Dogs of War', 180);
