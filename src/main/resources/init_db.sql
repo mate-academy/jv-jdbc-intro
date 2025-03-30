d6df52c58688a2b0444516c7abfb99f08f7d4873
Solvain <maluginm72@gmail.com>
2025-03-30 12:11:21 +0300

    Car

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,8 +1,10 @@
-CREATE DATABASE bookstore;
-USE bookstore;
-CREATE TABLE books
-(
-    id    BIGINT  AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255),
-    price DECIMAL(10, 2)
+
+CREATE TABLE car
+ (
+    id SERIAL PRIMARY KEY,
+    brand VARCHAR(50) NOT NULL,
+    model VARCHAR(50) NOT NULL,
+    number_plate VARCHAR(20) UNIQUE NOT NULL,
+    mileage INT NOT NULL CHECK (mileage >= 0),
+    was_in_accident BOOLEAN NOT NULL DEFAULT FALSE
 );
