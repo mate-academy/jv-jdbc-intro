be1aa1689804e004a851c2ec212d47379e13e298
Bohdan <111230201+FireFalcons@users.noreply.github.com>
2025-04-29 13:22:43 +0300

    added init_db file, Book, DataProcessingException, BookDao and BookDaoImpl clases and wrote the code implementation for them

---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,62 @@
+package mate.academy.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.ConnectionUtil;
+import mate.academy.lib.Dao;
+import mate.academy.model.Book;
+import mate.academy.model.DataProcessingException;
+
+@Dao
+public class BookDaoImpl implements BookDao {
+
+    @Override
+    public Book create(Book book) {
+        String sql = "INSERT INTO books (title,year) VALUES (?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection
+                        .prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, book.getTitle());
+            statement.setInt(2, book.getPrice().intValue());
+
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows < 1) {
+                throw new RuntimeException("Expected to insert at leas "
+                        + "one row, but inserted 0 rows");
+            }
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                Long id = generatedKeys.getObject(1, Long.class);
+                book.setId(id);
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can`t add new book", e);
+        }
+        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
+}
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,8 @@
+use bookTest;
+
+    CREATE TABLE books (
+        id INT NOT NULL AUTO_INCREMENT,
+        title VARCHAR(255),
+        price INT,
+        PRIMARY KEY (id)
+    );
