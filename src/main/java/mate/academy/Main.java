647748ef4d23ddc7bc0ed0f32b72e5ccbe25ade3
Egor <llikojia162@gmail.com>
2025-06-23 17:33:26 +0200

    - Establish connection to your Database. Be sure you have installed DB and you are able to create a new schema for this task. - Create `init_db.sql` file in `src/main/resources` folder. In this file, put the scripts for creating required table. - Create `Book` model. - Create DAO  layer for `Book` model. Below you can see the list of required methods. - You're already given an injector and `@Dao` annotation. Do not forget to use it for Dao implementations. - Return [Optional](https://docs.oracle.com/javase/8/docs/api/java/util/Optional.html) when you can return null in DAO.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,25 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("A");
+        book.setPrice(BigDecimal.valueOf(1000));
+        bookDao.create(book); //create
+        bookDao.findById(2L); //findById
+        bookDao.findAll(); //findAll
+        book.setId(5L);
+        book.setTitle("B");
+        book.setPrice(BigDecimal.valueOf(2000));
+        bookDao.update(book); //Update
+        bookDao.deleteById(10L); //Delete
     }
 }
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE books (
+    id INT PRIMARY KEY,
+    title VARCHAR(50),
+    price INT
+);
