e9a82756f63eb48836d0449e1145940c93828eea
Dmytro T <dmitriytverdokhlibov@yahoo.com>
2025-07-13 14:03:48 +0200

    implemented Book entity, BookDao interface, BookDaoImpl, and DataProcessingException

---
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -0,0 +1,19 @@
+package mate.academy.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/books_db?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "2021";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't connect to DB", e);
+        }
+    }
+}
