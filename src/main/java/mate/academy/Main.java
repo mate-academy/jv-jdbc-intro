7018e8fb3b43e4b2b5735769cad970bfa47a47fc
Dariusz <dariusz_bereszynski@op.pl>
2025-10-06 16:11:04 +0200

    First solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,32 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.Optional;
+import mate.academy.bookdao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("Mate");
+        book.setPrice(BigDecimal.valueOf(500));
+        bookDao.create(book);
+        Book book2 = new Book();
+        book2.setTitle("Academy");
+        book2.setPrice(BigDecimal.valueOf(200));
+        bookDao.create(book2);
+        Book book3 = new Book();
+        book3.setId(1L);
+        book3.setTitle("Mate");
+        book3.setPrice(BigDecimal.valueOf(400));
+        bookDao.update(book3);
+        Optional<Book> result = bookDao.findById(2L);
+        System.out.println(result);
+        bookDao.deleteById(1L);
+        bookDao.findAll();
     }
 }
