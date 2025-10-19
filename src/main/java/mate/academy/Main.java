0bdd466f2a03b5a5d137f15676fedf99aaf18d50
IGORECHEKST <165463152+IGORECHEKST@users.noreply.github.com>
2025-10-19 17:20:49 +0300

    ScSOL

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -8,8 +8,7 @@ import mate.academy.lib.Injector;
 import mate.academy.model.Book;
 
 public class Main {
-    private static final Injector injector = Injector
-            .getInstance("mate.academy");
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
     public static void main(String[] args) {
         BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
@@ -18,13 +17,14 @@ public class Main {
         Book book2 = new Book("Where the Crawdads Sing", new BigDecimal("14.99"));
 
         Book createdBook1 = bookDao.create(book1);
-        bookDao.create(book2);
+        Book createdBook2 = bookDao.create(book2);
 
         List<Book> allBooks = bookDao.findAll();
 
         Long idToFind = createdBook1.getId();
         Optional<Book> foundBook = bookDao.findById(idToFind);
 
+        // 4. UPDATE
         if (foundBook.isPresent()) {
             Book bookToUpdate = foundBook.get();
             bookToUpdate.setPrice(new BigDecimal("22.00"));
@@ -32,7 +32,7 @@ public class Main {
             bookDao.update(bookToUpdate);
         }
 
-        Long idToDelete = book2.getId();
+        Long idToDelete = createdBook2.getId();
         bookDao.deleteById(idToDelete);
 
         List<Book> finalBooks = bookDao.findAll();
