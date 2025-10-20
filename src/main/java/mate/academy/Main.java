3f98d1059f9f2d71156e150d9feddca25be76d1b
Kamil Jan Mularski <kammul@outlook.com>
2025-10-21 00:31:51 +0200

    Add BookDao CRUD operations in Main
    Main.java now demonstrates BookDao CRUD methods: create, update, deleteById, findById, and findAll. Book objects are initialized and manipulated to test DAO functionality.

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,5 +1,6 @@
 package mate.academy;
 
+import java.math.BigDecimal;
 import mate.academy.lib.Injector;
 import mate.academy.model.Book;
 import mate.academy.model.BookDao;
@@ -10,8 +11,21 @@ public class Main {
     public static void main(String[] args) {
         BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
         Book book = new Book();
-        // initialize field values using setters or constructor
+        book.setTitle("Potop");
+        book.setPrice(BigDecimal.valueOf(27.50));
         bookDao.create(book);
-        // test other methods from BookDao
+        book.setPrice(BigDecimal.valueOf(36.99));
+        bookDao.update(book);
+        bookDao.deleteById(book.getId());
+
+        bookDao.create(book);
+        book.setPrice(BigDecimal.valueOf(27.50));
+        bookDao.create(book);
+        book.setTitle("Krzyżacy");
+        book.setPrice(BigDecimal.valueOf(23.99));
+        bookDao.create(book);
+
+        System.out.println(bookDao.findById(3L));
+        System.out.println(bookDao.findAll());
     }
 }
