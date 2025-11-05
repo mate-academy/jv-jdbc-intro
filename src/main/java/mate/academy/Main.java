b91c82a68178e57d1357c8803f2cecd9b554e6e9
Yurii <kukharenko.yura@gmail.com>
2025-11-05 22:58:00 +0200

    Created all CRUD operations to solve the task

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,21 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy.dao");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book("Tom Sawyer", new BigDecimal("300.77"));
+        bookDao.create(book);
+        bookDao.findById(1L);
+        bookDao.findAll();
+        book.setPrice(new BigDecimal("500.25"));
+        bookDao.update(book);
+        bookDao.deleteById(1L);
     }
 }
