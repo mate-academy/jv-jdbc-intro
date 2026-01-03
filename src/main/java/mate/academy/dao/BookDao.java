4f8ee80cf985296916b4e15b7f8d0bd95758f36d
S0rbex <vitalikmartunushun@gmail.com>
2026-01-03 19:33:18 +0200

    Finish all task

---
+++ src/main/java/mate/academy/dao/BookDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import mate.academy.Objects.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookDao {
+    Book create(Book book);
+    Optional<Book> findById(Long id);
+    List<Book> findAll();
+    Book update(Book book);
+    boolean deleteById(Long id);
+}
---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,8 @@
+package dao;
+
+import mate.academy.lib.Dao;
+
+@Dao
+public class BookDaoImpl {
+
+}
