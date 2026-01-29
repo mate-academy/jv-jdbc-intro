88a7bd6ae2592cc95a1c7bc49605b02df8c66070
Kucherenko Oleksandr <amarokthegrey@gmail.com>
2026-01-29 17:55:37 +0800

    checkstyle fixes

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,9 +2,8 @@ package mate.academy;
 
 import mate.academy.dao.BookDao;
 import mate.academy.lib.Injector;
-import mate.academy.model.Book;
-
 import java.math.BigDecimal;
+import mate.academy.model.Book;
 
 public class Main {
     private static final Injector injector =
