74a51014ce8e8ec70dcbf08a2febbc2586e1037e
alex <ursusandwolf@protonmail.com>
2026-02-26 15:10:14 +0200

    main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,17 +1,34 @@
 package mate.academy;
 
 import mate.academy.dao.BookDao;
+import mate.academy.lib.ConnectionUtil;
 import mate.academy.lib.Injector;
+import mate.academy.model.Book;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * У main отримай BookDao через injector і протестуй всі CRUD-методи:
  * create → findById → findAll → update → deleteById.
  */
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        BookDao bookDao;
-        Injector injector = Injector.getInstance("mate.academy");
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        // initialize field values using setters or constructor
+        bookDao.create(book);
+        // test other methods from BookDao
 
         //bookDao.create(new Book());
+        Connection connection = ConnectionUtil.getConnection();
+        System.out.println(connection);
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
