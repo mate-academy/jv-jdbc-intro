3eb6bab3b2e30bf5bf994e1035967a4be6f711c4
hero <antonstendal19@gmail.com>
2026-03-04 06:38:21 +0100

    Add Book entity and DAO layer implementation

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,27 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("I'm legend 2");
+        book.setPrice(BigDecimal.valueOf(55.99));
+        bookDao.create(book);
+
+        Book book1 = new Book();
+        book.setTitle("Spirit retern");
+        book.setPrice(BigDecimal.valueOf(15));
+        bookDao.update(book1);
+        bookDao.findAll();
+        bookDao.findById(1L);
+        bookDao.deleteById(4L);
     }
 }
+
---
+++ src/main/java/mate/academy/dao/BookDaoImpl.java
@@ -0,0 +1,126 @@
+package mate.academy.dao;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.exceptions.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Book;
+
+@Dao
+public class BookDaoImpl implements BookDao {
+    @Override
+    public Book create(Book book) {
+        String sql = "INSERT INTO books (title, price) VALUES (?, ?)";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql,
+                        Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, book.getTitle());
+            statement.setBigDecimal(2, book.getPrice());
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new DataProcessingException("Creating book failed, no rows affected.");
+            }
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    book.setId(generatedKeys.getLong(1));
+                } else {
+                    throw new DataProcessingException("Creating book failed, no ID obtained.");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't add a new book " + book, e);
+        }
+        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long id) {
+        String sql = "SELECT * FROM books WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    String title = resultSet.getString("title");
+                    BigDecimal price = resultSet.getBigDecimal("price");
+
+                    Book book = new Book();
+                    book.setId(id);
+                    book.setTitle(title);
+                    book.setPrice(price);
+                    return Optional.of(book);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't find book by id " + id, e);
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Book> findAll() {
+        String sql = "SELECT * FROM books";
+        List<Book> allBooks = new ArrayList<>();
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+
+                while (resultSet.next()) {
+                    Book book = new Book();
+                    book.setId(resultSet.getLong("id"));
+                    book.setTitle(resultSet.getString("title"));
+                    book.setPrice(resultSet.getBigDecimal("price"));
+                    allBooks.add(book);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't find books", e);
+        }
+        return allBooks;
+    }
+
+    @Override
+    public Book update(Book book) {
+        if (book.getId() == null) {
+            throw new DataProcessingException("Book id is null");
+        }
+        String sql = "UPDATE books SET title = ?, price = ? WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, book.getTitle());
+            statement.setBigDecimal(2, book.getPrice());
+            statement.setLong(3, book.getId());
+            int affectedRows = statement.executeUpdate();
+            if (affectedRows == 0) {
+                throw new DataProcessingException("Updating book failed, no rows affected.");
+            }
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't update book "
+                    + book + " (id= " + book.getId() + ")", e);
+        }
+        return book;
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        if (id == null) {
+            throw new DataProcessingException("Book id is null");
+        }
+        String sql = "DELETE FROM books WHERE id = ?";
+        try (Connection connection = ConnectionUtil.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, id);
+            int affectedRows = statement.executeUpdate();
+            return affectedRows > 0;
+        } catch (SQLException e) {
+            throw new DataProcessingException("Can't delete book by id " + id, e);
+        }
+    }
+}
