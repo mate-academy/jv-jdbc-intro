055e6c9f90f5d63cb277172c17a16a7d3f9d7465
Nikita Kravtsov <deez999deez@gmail.com>
2026-03-17 16:18:44 +0200

    Implement books table and DAO pattern
    Create:
    - 'book' table with columns: id, title, price;
    - Book model with fields: id, title, price;
    - BookDao and BookDaoImpl with CRUD operations: create, findById, findAll, update, delete;
    - custom exception DataProcessingException for Dao implementation
    - ConnectionUtil class for DB connection
    
    Add:
    - init_db.sql resource with scripts for creating table
    - Maven dependencies for MySQL connection

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,38 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final String BOOK_TITLE_EXAMPLE = "Lords of the Rings";
+    private static final String BOOK_TITLE_UPDATE = "Harry Potter";
+    private static final BigDecimal BOOK_PRICE_EXAMPLE = new BigDecimal("99.99");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle(BOOK_TITLE_EXAMPLE);
+        book.setPrice(BOOK_PRICE_EXAMPLE);
+
+        Book bookCreate = bookDao.create(book);
+        System.out.println(bookCreate);
+
+        Optional<Book> bookFindById = bookDao.findById(bookCreate.getId());
+        System.out.println(bookFindById);
+
+        List<Book> booksFindAll = bookDao.findAll();
+        System.out.println(booksFindAll);
+
+        book.setTitle(BOOK_TITLE_UPDATE);
+        Optional<Book> bookUpdate = bookDao.update(book);
+        System.out.println(bookUpdate);
+
+        boolean bookDelete = bookDao.deleteById(bookCreate.getId());
+        System.out.println(bookDelete);
     }
 }
