829c9bd42eefcb5c05a93e82603669026d28bf19
Denya Basarab <denya2007basarab@gmail.com>
2026-03-18 15:02:28 +0200

    feat: add main method for testing book DaoImpl

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,27 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import java.util.Optional;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+        Book book = new Book();
+        book.setTitle("COLOBOK");
+        book.setPrice(BigDecimal.valueOf(123));
+
+        bookDao.create(book);
+        Optional<Book> byId = bookDao.findById(2L);
+        Book book1 = byId.get();
+        book1.setPrice(BigDecimal.valueOf(555));
+        bookDao.update(book1);
+        System.out.println(bookDao.findAll());
+        bookDao.deleteById(3L);
+        System.out.println(bookDao.findAll());
     }
 }
