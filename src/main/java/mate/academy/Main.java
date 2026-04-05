5da1f845d328058f73bfa4dee38052f9eed77d70
PatrykWski <patrykwiszniewski96@gmail.com>
2026-04-05 14:47:52 +0200

    preparing to make connection

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,32 @@
 package mate.academy;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Properties dbProporties = new Properties();
+            dbProporties.put("user", "root");
+            dbProporties.put("password", "12345");
+            Connection connection =
+                    DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", dbProporties);
+            String sql = "SELECT * FROM car WHERE id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, 1L);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
 
+            }
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't load MSQL file", e);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection", e);
+        }
     }
 }
---
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -0,0 +1,5 @@
+package mate.academy.util;
+
+public class ConnectionUtil {
+    Class.f
+}
