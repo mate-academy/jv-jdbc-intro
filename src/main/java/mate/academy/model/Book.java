a69c63a704a4e469ec9c80885c8cdaf6ba633d35
artem <artemnurmetow@gmail.com>
2026-04-16 23:09:22 +0200

    - added Book model and BookDao Interface with all methods -added custom exception -connected Database to Project

---
+++ src/main/java/mate/academy/model/Book.java
@@ -0,0 +1,12 @@
+package mate.academy.model;
+
+import java.math.BigDecimal;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+}
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,5 @@
+CREATE TABLE books(
+    id int PRIMARY KEY,
+    title varchar(255),
+    price float
+)
