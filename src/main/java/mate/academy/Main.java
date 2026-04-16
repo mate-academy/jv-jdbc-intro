b4ae5b5341b58d19656aa01c12266b142bf26b32
4Empty4 <jumanji2228@gmail.com>
2026-04-16 18:00:50 +0300

    Add Book DAO implementation and database connection

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,34 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        Book book = new Book();
+        book.setTitle("Harry Potter");
+        book.setPrice(BigDecimal.valueOf(250));
+
+        Book createdBook = bookDao.create(book);
+        System.out.println("Created book: " + createdBook);
+
+        System.out.println("Find by id: " + bookDao.findById(createdBook.getId()));
+        System.out.println("Find all: " + bookDao.findAll());
+
+        createdBook.setTitle("Harry Potter and the Philosopher's Stone");
+        createdBook.setPrice(BigDecimal.valueOf(300));
+        Book updatedBook = bookDao.update(createdBook);
+        System.out.println("Updated book: " + updatedBook);
+
+        boolean isDeleted = bookDao.deleteById(updatedBook.getId());
+        System.out.println("Deleted: " + isDeleted);
+
+        System.out.println("Find after delete: " + bookDao.findById(updatedBook.getId()));
     }
 }
---
+++ src/main/java/mate/academy/util/ConnectionUtil.java
@@ -0,0 +1,22 @@
+package mate.academy.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/book_store";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    private ConnectionUtil() {
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection to DB", e);
+        }
+    }
+}
