0d7100af46b9320da2108ec8f735e5eb5cb69524
VadimN637 <turnedofff@gmail.com>
2026-05-07 16:44:13 +0300

    4

--- src/main/java/mate/academy/dao/impl/BookDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/BookDaoImpl.java
@@ -8,7 +8,6 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import mate.academy.dao.BookDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
