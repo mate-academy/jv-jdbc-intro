c9c6167df88fbc420ccbbc182d0fbb0f4c15965d
obvly <zazedu4@gmail.com>
2026-05-24 20:58:24 +0200

    First upload

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,36 @@
 package mate.academy;
 
-public class Main {
-    public static void main(String[] args) {
+import java.math.BigDecimal;
+import mate.academy.dao.BookDao;
+import mate.academy.lib.Injector;
+import mate.academy.model.Book;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    public static void main(String[] args) {
+        BookDao bookDao = (BookDao) injector.getInstance(BookDao.class);
+
+        // Create a book
+        Book book = new Book();
+        book.setTitle("Thinking in Java");
+        book.setPrice(BigDecimal.valueOf(450.50));
+        bookDao.create(book);
+        System.out.println("1. Book is created: " + book);
+
+        //Find it
+        System.out.println("2. Searching by Id: " + bookDao.findById(book.getId()));
+
+        // Update the price
+        book.setPrice(BigDecimal.valueOf(500.00));
+        bookDao.update(book);
+        System.out.println("3. After price updating: " + bookDao.findById(book.getId()));
+
+        // Show out
+        System.out.println("4. List of all books: " + bookDao.findAll());
+
+        //Deleting
+        boolean deleted = bookDao.deleteById(book.getId());
+        System.out.println("5. Is deleting successfully?  " + deleted);
     }
 }
