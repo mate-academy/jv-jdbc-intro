32434f9ea9ac167f40ee0c6450346ffa115901e0
Michal Brynkiewicz <michalbry@autograf.pl>
2026-07-04 00:13:36 +0200

    JDBC v2

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,12 +1,5 @@
-CREATE TABLE IF NOT EXISTS test.books (
+CREATE TABLE IF NOT EXISTS books (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL
     );
-
-
-DELETE from books where id in (2,3,4,5,6);
-
-truncate table books;
-
-select * from books;
