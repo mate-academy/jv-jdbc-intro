cdeb78529899da56c1683da6dba41fbe65d48176
DoctorJambo <arhipov09090az@gmail.com>
2026-07-23 18:46:29 +0300

    fix3

--- src/main/java/mate/academy/DataProcessingException.java
+++ src/main/java/mate/academy/DataProcessingException.java
@@ -1,7 +1,5 @@
 package mate.academy;
 
-import java.sql.SQLException;
-
 public class DataProcessingException extends RuntimeException {
     public DataProcessingException(String massage, Throwable e) {
         super(massage, e);
